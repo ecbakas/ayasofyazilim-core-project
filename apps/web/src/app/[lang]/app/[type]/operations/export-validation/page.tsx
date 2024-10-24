9f2cd462d672e491e4df071bb3d29801cd134bd8
yusualhashash <yusufalhashash2018@gmail.com>
2024-10-24 11:57:01 +0300

    feat: add the main page and table for Export Validation

---
+++ apps/web/src/app/[lang]/app/[type]/operations/export-validation/page.tsx
@@ -0,0 +1,44 @@
+"use server";
+import { $UniRefund_ExportValidationService_ExportValidations_ExportValidationDto } from "@ayasofyazilim/saas/ExportValidationService";
+import TableComponent from "@repo/ui/TableComponent";
+import { getResourceData } from "src/language-data/ExportValidationService";
+import { getTableData } from "../../../actions/api-requests";
+
+export default async function Page({ params }: { params: { lang: string } }) {
+  const { languageData } = await getResourceData(params.lang);
+
+  return (
+    <TableComponent
+      fetchRequest={async (page) => {
+        "use server";
+        const response = await getTableData("export-validation", page, 10);
+        if (response.type === "success") {
+          const data = response.data;
+          return {
+            type: "success",
+            data: { items: data.items || [], totalCount: data.totalCount || 0 },
+          };
+        }
+        return {
+          type: "success",
+          data: { items: [], totalCount: 0 },
+        };
+      }}
+      languageData={languageData}
+      tableSchema={{
+        excludeList: [
+          "isDeleted",
+          "deleterId",
+          "deletionTime",
+          "lastModificationTime",
+          "lastModifierId",
+          "creationTime",
+          "creatorId",
+          "id",
+        ],
+        schema:
+          $UniRefund_ExportValidationService_ExportValidations_ExportValidationDto,
+      }}
+    />
+  );
+}
