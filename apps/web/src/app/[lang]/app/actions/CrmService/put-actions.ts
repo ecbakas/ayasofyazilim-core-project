25a82b4fe8b1b2ca00d61aa4a3731cdc4997baf4
Eren <ahmeterenodaci@gmail.com>
2024-10-30 11:06:17 +0300

    feat(crm): add putCrmEmailAddressApi

--- apps/web/src/app/[lang]/app/actions/CrmService/put-actions.ts
+++ apps/web/src/app/[lang]/app/actions/CrmService/put-actions.ts
@@ -3,6 +3,7 @@
 import type {
   PutApiCrmServiceMerchantsByIdAddressesByAddressIdData,
   PutApiCrmServiceMerchantsByIdData,
+  PutApiCrmServiceMerchantsByIdEmailsByEmailIdData,
 } from "@ayasofyazilim/saas/CRMService";
 import { structuredError } from "src/lib";
 import { getApiRequests } from "../api-requests";
@@ -37,6 +38,28 @@ export async function putCrmAddressApi(
     return {
       type: "success" as const,
       data: await requests[partyName].putAddress(data),
+      status: 200,
+      message: "",
+    };
+  } catch (error) {
+    return structuredError(error);
+  }
+}
+
+export async function putCrmEmailAddressApi(
+  partyName:
+    | "merchants"
+    | "refund-points"
+    | "customs"
+    | "tax-offices"
+    | "tax-free",
+  data: PutApiCrmServiceMerchantsByIdEmailsByEmailIdData,
+) {
+  try {
+    const requests = await getApiRequests();
+    return {
+      type: "success" as const,
+      data: await requests[partyName].putEmailAddress(data),
       status: 200,
       message: "",
     };
---
+++ apps/web/src/app/[lang]/app/actions/CrmService/types.ts
@@ -0,0 +1,4 @@
+import type { UniRefund_CRMService_EmailCommonDatas_UpdateEmailCommonDataDto } from "@ayasofyazilim/saas/CRMService";
+
+export type EmailUpdateDto =
+  UniRefund_CRMService_EmailCommonDatas_UpdateEmailCommonDataDto;
