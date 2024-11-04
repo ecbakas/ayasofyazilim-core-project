2fefa52acfab8038a71d2a38309766305b12fccb
yusualhashash <yusufalhashash2018@gmail.com>
2024-11-04 16:19:02 +0300

    refcator: use  create traveller function without components

--- apps/web/src/app/[lang]/app/[type]/parties/traveller/new/form.tsx
+++ apps/web/src/app/[lang]/app/[type]/parties/traveller/new/form.tsx
@@ -1,223 +1,83 @@
 "use client";
 
 import { toast } from "@/components/ui/sonner";
-import type {
-  UniRefund_LocationService_Cities_CityDto,
-  UniRefund_LocationService_Countries_CountryDto,
-} from "@ayasofyazilim/saas/LocationService";
-import type { UniRefund_TravellerService_Travellers_CreateWithComponentsTravellerDto as travellerCreateDTOType } from "@ayasofyazilim/saas/TravellerService";
+import type { UniRefund_TravellerService_Travellers_CreateTravellerDto as TravellerDto } from "@ayasofyazilim/saas/TravellerService";
+import { $UniRefund_TravellerService_Travellers_CreateTravellerDto } from "@ayasofyazilim/saas/TravellerService";
 import { createZodObject } from "@repo/ayasofyazilim-ui/lib/create-zod-object";
-import type { AutoFormInputComponentProps } from "@repo/ayasofyazilim-ui/organisms/auto-form";
 import AutoForm, {
   AutoFormSubmit,
   createFieldConfigWithResource,
   CustomCombobox,
-  mergeFieldConfigs,
 } from "@repo/ayasofyazilim-ui/organisms/auto-form";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import type { CountryDto } from "src/app/[lang]/app/actions/LocationService/types";
 import { postTravellerApi } from "src/app/[lang]/app/actions/TravellerService/post-actions";
 import type { TravellerServiceResource } from "src/language-data/TravellerService";
 import { getBaseLink } from "src/utils";
-import { isPhoneValid, splitPhone } from "src/utils-phone";
-import {
-  getCitiesApi,
-  getCountriesApi,
-} from "../../../../actions/LocationService/actions";
-import type { CreateTravellerDTO } from "../data";
-import {
-  createTravellerSchema,
-  formPositions,
-  formSubPositions,
-} from "../data";
+
+const TravellerSchema = createZodObject(
+  $UniRefund_TravellerService_Travellers_CreateTravellerDto,
+);
 
 export default function Form({
   languageData,
+  countryList,
 }: {
   languageData: TravellerServiceResource;
+  countryList: CountryDto[];
 }) {
   const router = useRouter();
-  const [cities, setCities] = useState<
-    UniRefund_LocationService_Cities_CityDto[]
-  >([]);
-  const [countries, setCountries] = useState<
-    UniRefund_LocationService_Countries_CountryDto[]
-  >([]);
-
-  const getCity = async () => {
-    try {
-      const response = await getCitiesApi({ maxResultCount: 1000 });
-      if (response.type === "success") {
-        setCities(response.data.items || []);
-      } else {
-        toast.error(response.message);
-      }
-    } catch (error) {
-      toast.error(`traveller/new:${languageData["Fetch.Fail.City"]}`);
-    }
-  };
-
-  const getCountry = async () => {
-    try {
-      const response = await getCountriesApi();
-      if (response.type === "success") {
-        setCountries(response.data.items || []);
-      } else {
-        toast.error(response.message);
-      }
-    } catch (error) {
-      toast.error(
-        `traveller/new/country: ${languageData["Fetch.Fail.Country"]}`,
-      );
-    }
-  };
-
-  useEffect(() => {
-    void getCity();
-    void getCountry();
-  }, []);
-
-  const SaveTraveller = async (formData: CreateTravellerDTO) => {
-    const isValid = isPhoneValid(formData.telephone.localNumber);
-    if (!isValid) {
-      return;
-    }
-    const phoneData = splitPhone(formData.telephone.localNumber);
-    formData.telephone = { ...formData.telephone, ...phoneData };
-    const createformData: travellerCreateDTOType = {
-      entityInformationTypes: [
-        {
-          individuals: [
-            {
-              name: {
-                ...formData.name,
-              },
-              personalSummaries: [
-                {
-                  ...formData.personalSummaries,
-                },
-              ],
-              contactInformations: [
-                {
-                  telephones: [{ ...formData.telephone, primaryFlag: true }],
-                  emails: [{ ...formData.email, primaryFlag: true }],
-                  addresses: [
-                    {
-                      ...formData.address,
-                      primaryFlag: true,
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-      personalIdentificationCommonDatas: [
-        {
-          ...formData.personalIdentificationCommonDatas,
-        },
-      ],
-      personalPreferencesTypes: [
-        {
-          ...formData.personalPreferencesTypes,
-        },
-      ],
-    };
-    try {
-      const response = await postTravellerApi({
-        requestBody: createformData,
-      });
-      if (response.type === "error" || response.type === "api-error") {
-        toast.error(response.message || languageData["Travellers.New.Error"]);
-      } else {
-        toast.success(languageData["Travellers.New.Succes"]);
-        router.push(getBaseLink(`/app/admin/parties/traveller`));
-      }
-    } catch (error) {
-      toast.error(languageData["Travellers.Fail"]);
-    }
-  };
 
   const translatedForm = createFieldConfigWithResource({
-    schema: createTravellerSchema,
+    schema: $UniRefund_TravellerService_Travellers_CreateTravellerDto,
     resources: languageData,
+    extend: {
+      personalIdentification: {
+        residenceCountryCode2: {
+          renderer: (props) => (
+            <CustomCombobox<CountryDto>
+              childrenProps={props}
+              emptyValue={languageData["Country.Select"]}
+              list={countryList}
+              selectIdentifier="code2"
+              selectLabel="name"
+            />
+          ),
+        },
+        nationalityCountryCode2: {
+          renderer: (props) => (
+            <CustomCombobox<CountryDto>
+              childrenProps={props}
+              emptyValue={languageData["Country.Select"]}
+              list={countryList}
+              selectIdentifier="code2"
+              selectLabel="name"
+            />
+          ),
+        },
+      },
+    },
   });
-
-  const countryEnum = {
-    renderer: (props: AutoFormInputComponentProps) => (
-      <CustomCombobox<UniRefund_LocationService_Countries_CountryDto>
-        childrenProps={props}
-        emptyValue={languageData["Country.Select"]}
-        list={countries}
-        selectIdentifier="code2"
-        selectLabel="name"
-      />
-    ),
-  };
+  async function createTraveller(data: TravellerDto) {
+    const response = await postTravellerApi({ requestBody: data });
+    if (response.type === "error" || response.type === "api-error") {
+      toast.error(response.message || languageData["Travellers.New.Error"]);
+    } else {
+      toast.success([languageData["Travellers.New.Succes"]]);
+      router.push(getBaseLink(`/app/admin/parties/traveller`));
+    }
+  }
 
   return (
     <AutoForm
-      className=" grid gap-2 space-y-0 md:grid-cols-2 lg:grid-cols-3"
-      fieldConfig={mergeFieldConfigs(translatedForm, {
-        telephone: {
-          localNumber: {
-            fieldType: "phone",
-            inputProps: {
-              showLabel: true,
-            },
-          },
-        },
-        email: {
-          emailAddress: {
-            inputProps: {
-              type: "email",
-            },
-          },
-        },
-        address: {
-          className: "row-span-2",
-          city: {
-            renderer: (props: AutoFormInputComponentProps) => (
-              <CustomCombobox<UniRefund_LocationService_Cities_CityDto>
-                childrenProps={props}
-                emptyValue={languageData["City.Select"]}
-                list={cities}
-                selectIdentifier="id"
-                selectLabel="name"
-              />
-            ),
-          },
-          country: {
-            renderer: (props: AutoFormInputComponentProps) => (
-              <CustomCombobox<UniRefund_LocationService_Countries_CountryDto>
-                childrenProps={props}
-                emptyValue={languageData["Country.Select"]}
-                list={countries}
-                selectIdentifier="id"
-                selectLabel="name"
-              />
-            ),
-          },
-        },
-        personalIdentificationCommonDatas: {
-          className: "row-span-2",
-          residenceCountryCode2: countryEnum,
-          nationalityCountryCode2: countryEnum,
-        },
-      })}
-      formSchema={createZodObject(
-        createTravellerSchema,
-        formPositions,
-        undefined,
-        formSubPositions,
-      )}
+      fieldConfig={translatedForm}
+      formSchema={TravellerSchema}
       onSubmit={(val) => {
-        void SaveTraveller(val as CreateTravellerDTO);
+        void createTraveller(val as TravellerDto);
       }}
       stickyChildren
     >
-      <AutoFormSubmit className="float-right py-4">
+      <AutoFormSubmit className="float-right px-8 py-4">
         {languageData.Save}
       </AutoFormSubmit>
     </AutoForm>
--- apps/web/src/app/[lang]/app/[type]/parties/traveller/new/page.tsx
+++ apps/web/src/app/[lang]/app/[type]/parties/traveller/new/page.tsx
@@ -1,9 +1,14 @@
 "use server";
 
 import { getResourceData } from "src/language-data/TravellerService";
+import { getCountriesApi } from "src/app/[lang]/app/actions/LocationService/actions";
 import Form from "./form";
 
 export default async function Page({ params }: { params: { lang: string } }) {
   const { languageData } = await getResourceData(params.lang);
-  return <Form languageData={languageData} />;
+  const Countries = await getCountriesApi();
+  const CountryList =
+    (Countries.type === "success" && Countries.data.items) || [];
+
+  return <Form countryList={CountryList} languageData={languageData} />;
 }
