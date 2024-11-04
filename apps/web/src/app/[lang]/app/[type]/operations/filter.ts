955059a1480939cc2d2cd6a632d544e8d36768c6
yusualhashash <yusufalhashash2018@gmail.com>
2024-11-04 14:14:40 +0300

    refactor: move the filter from tag to the outside to use it in export validation page too

---
+++ apps/web/src/app/[lang]/app/[type]/operations/filter.ts
@@ -0,0 +1,110 @@
+import type { GetApiTagServiceTagData } from "@ayasofyazilim/saas/TagService";
+import {
+  $UniRefund_TagService_Tags_RefundType,
+  $UniRefund_TagService_Tags_TagStatusType,
+} from "@ayasofyazilim/saas/TagService";
+import type { ColumnFilter } from "@repo/ayasofyazilim-ui/molecules/tables";
+
+export type FilterType = Partial<keyof GetApiTagServiceTagData>;
+export type DetailedFilter = ColumnFilter & { name: FilterType };
+export type TypedFilter = Partial<Record<FilterType, DetailedFilter>>;
+
+export const CommonFilter: TypedFilter = {
+  exportEndDate: {
+    name: "exportEndDate",
+    displayName: "Export End Date",
+    type: "date",
+    value: "",
+  },
+  exportStartDate: {
+    name: "exportStartDate",
+    displayName: "Export End Date",
+    type: "date",
+    value: "",
+  },
+  issuedEndDate: {
+    name: "issuedEndDate",
+    displayName: "Issued End Date",
+    type: "date",
+    value: "",
+  },
+  invoiceNumber: {
+    name: "invoiceNumber",
+    displayName: "Invoice Number",
+    type: "string",
+    value: "",
+  },
+  tagNumber: {
+    name: "tagNumber",
+    displayName: "Tag Number",
+    type: "string",
+    value: "",
+  },
+  sorting: {
+    name: "sorting",
+    displayName: "Sorting",
+    type: "select",
+    value: "",
+    options: [
+      { label: "Ascending", value: "asc" },
+      { label: "Descending", value: "desc" },
+    ],
+    placeholder: "Select Sorting",
+  },
+  paidEndDate: {
+    name: "paidEndDate",
+    displayName: "Paid End Date",
+    type: "date",
+    value: "",
+  },
+  issuedStartDate: {
+    name: "issuedStartDate",
+    displayName: "Issued Start Date",
+    type: "date",
+    value: "",
+  },
+  paidStartDate: {
+    name: "paidStartDate",
+    displayName: "Paid Start Date",
+    type: "date",
+    value: "",
+  },
+  refundTypes: {
+    name: "refundTypes",
+    displayName: "Refund Types",
+    type: "select",
+    options: [
+      ...$UniRefund_TagService_Tags_RefundType.enum.map((item) => ({
+        label: item,
+        value: item,
+      })),
+    ],
+    placeholder: "Select Refund Type",
+    value: "",
+  },
+  statuses: {
+    name: "statuses",
+    displayName: "Statuses",
+    type: "select",
+    value: "",
+    options: [
+      ...$UniRefund_TagService_Tags_TagStatusType.enum.map((item) => ({
+        label: item,
+        value: item,
+      })),
+    ],
+    placeholder: "Select Status",
+  },
+  travellerDocumentNumber: {
+    name: "travellerDocumentNumber",
+    displayName: "Traveller Document Number",
+    type: "string",
+    value: "",
+  },
+  travellerFullName: {
+    name: "travellerFullName",
+    displayName: "Traveller Name",
+    type: "string",
+    value: "",
+  },
+};
