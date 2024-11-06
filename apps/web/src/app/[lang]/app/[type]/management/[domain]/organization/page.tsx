05474302a5f7b3e04c9f4d4fe8f0cce42ca5216c
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-11-06 14:02:23 +0300

    chore: remove extra import

--- apps/web/src/app/[lang]/app/[type]/management/[domain]/organization/page.tsx
+++ apps/web/src/app/[lang]/app/[type]/management/[domain]/organization/page.tsx
@@ -17,7 +17,8 @@ import {
   $Volo_Abp_Identity_OrganizationUnitUpdateDto,
 } from "@ayasofyazilim/saas/IdentityService";
 import Button from "@repo/ayasofyazilim-ui/molecules/button";
-import AutoformDialog, { TableActionCustomDialog } from "@repo/ayasofyazilim-ui/molecules/dialog";
+import type { TableActionCustomDialog } from "@repo/ayasofyazilim-ui/molecules/dialog";
+import AutoformDialog from "@repo/ayasofyazilim-ui/molecules/dialog";
 import { TreeView } from "@repo/ayasofyazilim-ui/molecules/tree-view";
 import { SectionNavbarBase } from "@repo/ayasofyazilim-ui/templates/section-layout";
 import { Trash2 } from "lucide-react";
@@ -33,7 +34,6 @@ import {
   fetchUsersForUnit,
 } from "./action";
 import { ConfirmDialog, RoleModal, UserModal } from "./form";
-import { TableAction } from "@repo/ayasofyazilim-ui/molecules/tables/types";
 
 function getChildrens(parentId: string, data: OrganizationUnit[]) {
   const childrens: TreeViewElement[] = [];
@@ -88,7 +88,9 @@ const App: React.FC = () => {
   >([]);
   const [selectedUnitId, setSelectedUnitId] = useState<string | undefined>();
   const [open, setOpen] = useState(false);
-  const [action, setAction] = useState<TableActionCustomDialog | undefined>(undefined);
+  const [action, setAction] = useState<TableActionCustomDialog | undefined>(
+    undefined,
+  );
   const [unitUsers, setUnitUsers] = useState<User[]>([]);
   const [unitRoles, setUnitRoles] = useState<Role[]>([]);
   const [isUserModalOpen, setIsUserModalOpen] = useState(false);
