b0d8ac190cc76e0a4563190b1482dc61e87d6b8d
yusualhashash <yusufalhashash2018@gmail.com>
2024-11-13 15:49:28 +0300

    feat: add get details backend function

--- apps/web/src/app/[lang]/app/actions/FinanceService/actions.ts
+++ apps/web/src/app/[lang]/app/actions/FinanceService/actions.ts
@@ -1,12 +1,11 @@
 "use server";
-import type { GetApiFinanceServiceBillingsData } from "@ayasofyazilim/saas/FinanceService";
 import { structuredError, structuredResponse } from "src/lib";
 import { getApiRequests } from "../api-requests";
 
-export async function getBillingApi(data: GetApiFinanceServiceBillingsData) {
+export async function getBillingDetailApi(id: string) {
   try {
     const requests = await getApiRequests();
-    const dataResponse = await requests.billing.get(data);
+    const dataResponse = await requests.billing.getDetail(id);
     return structuredResponse(dataResponse);
   } catch (error) {
     return structuredError(error);
