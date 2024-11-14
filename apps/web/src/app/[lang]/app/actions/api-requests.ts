e986a3703d0af0a30d120beefcbf3226848db328
yusualhashash <yusufalhashash2018@gmail.com>
2024-11-14 15:37:47 +0300

    feat: add the put change password function in backend

--- apps/web/src/app/[lang]/app/actions/api-requests.ts
+++ apps/web/src/app/[lang]/app/actions/api-requests.ts
@@ -47,6 +47,7 @@ import type {
   GetApiIdentityUsersData,
   PutApiIdentityRolesByIdClaimsData,
   PutApiIdentityRolesByIdMoveAllUsersData,
+  PutApiIdentityUsersByIdChangePasswordData,
   PutApiIdentityUsersByIdClaimsData,
   PutApiOpeniddictApplicationsByIdTokenLifetimeData,
 } from "@ayasofyazilim/saas/IdentityService";
@@ -540,6 +541,10 @@ export async function getApiRequests() {
         await identityClient.user.getApiIdentityUsersByIdClaims(data),
       putUserClaims: async (data: PutApiIdentityUsersByIdClaimsData) =>
         await identityClient.user.putApiIdentityUsersByIdClaims(data),
+      "change-password": async (
+        data: PutApiIdentityUsersByIdChangePasswordData,
+      ) =>
+        await identityClient.user.putApiIdentityUsersByIdChangePassword(data),
     },
     editions: {
       getAllEditions: async () =>
