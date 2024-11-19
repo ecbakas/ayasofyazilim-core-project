4f146bcb945654f0bce4a6bb28eb5a3ae70b0a68
yusualhashash <yusufalhashash2018@gmail.com>
2024-11-19 11:03:02 +0300

    feat: add the fetch function to get the data in autoform

--- apps/web/src/app/[lang]/app/[type]/management/data.tsx
+++ apps/web/src/app/[lang]/app/[type]/management/data.tsx
@@ -61,6 +61,7 @@ import type { DataConfigArray } from "src/types";
 import { getBaseLink } from "src/utils";
 import {
   getAllRolesApi,
+  getApplicationTokenLifetimeApi,
   moveAllUsersApi,
 } from "../../actions/IdentityService/actions";
 import {
@@ -174,17 +175,15 @@ export const dataConfig: DataConfigArray = {
               autoFormArgs: {
                 preFetch: {
                   functionCall: async (triggerData) => {
-                    await Promise.resolve();
                     const _triggerData =
                       triggerData as Volo_Abp_OpenIddict_Applications_Dtos_ApplicationDto;
-                    const returnValues: Volo_Abp_OpenIddict_Applications_Dtos_ApplicationTokenLifetimeDto =
-                      {
-                        accessTokenLifetime: 0,
-                        authorizationCodeLifetime: 2,
-                        identityTokenLifetime: Number(
-                          _triggerData.extraProperties,
-                        ),
-                      };
+
+                    const returnValues = await getApplicationTokenLifetimeApi({
+                      id: _triggerData.id || "",
+                    }).then(
+                      (response) =>
+                        response.data as Volo_Abp_OpenIddict_Applications_Dtos_ApplicationTokenLifetimeDto,
+                    );
                     return returnValues;
                   },
                 },
