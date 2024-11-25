92f7d36beba2cbd9906f38110e851229b757c441
yusualhashash <yusufalhashash2018@gmail.com>
2024-11-25 09:55:37 +0300

    feat: add frm of create new identification

---
+++ apps/web/src/app/[lang]/app/[type]/parties/traveller/[travellerId]/identification/new/page.tsx
@@ -0,0 +1,44 @@
+"use server";
+
+import type { UniRefund_TravellerService_Travellers_TravellerDetailProfileDto } from "@ayasofyazilim/saas/TravellerService";
+import { getResourceData } from "src/language-data/TravellerService";
+import { getTravellersDetailsApi } from "src/app/[lang]/app/actions/TravellerService/actions";
+import { getCountriesApi } from "src/app/[lang]/app/actions/LocationService/actions";
+import { getBaseLink } from "src/utils";
+import Form from "./form";
+
+export default async function Page({
+  params,
+}: {
+  params: { travellerId: string; lang: string };
+}) {
+  const { languageData } = await getResourceData(params.lang);
+  const Traveller = await getTravellersDetailsApi(params.travellerId);
+  const countries = await getCountriesApi();
+  const countryList =
+    (countries.type === "success" && countries.data.items) || [];
+  const travellerData =
+    Traveller.data as UniRefund_TravellerService_Travellers_TravellerDetailProfileDto;
+
+  return (
+    <>
+      <Form
+        countryList={{
+          data: countryList,
+          success: countries.type === "success",
+        }}
+        languageData={languageData}
+        travellerId={params.travellerId}
+      />
+      <div className="hidden" id="page-title">
+        {`${languageData.Traveller} (${travellerData.personalIdentifications[0].fullName})`}
+      </div>
+      <div className="hidden" id="page-description">
+        {languageData["Travellers.Create.Identification.Description"]}
+      </div>
+      <div className="hidden" id="page-back-link">
+        {getBaseLink(`/app/admin/parties/traveller/${params.travellerId}`)}
+      </div>
+    </>
+  );
+}
