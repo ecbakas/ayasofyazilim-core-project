177950e0fdcc65986fc9d51ca403f2dd12db31e8
yusualhashash <yusufalhashash2018@gmail.com>
2024-11-30 01:46:48 +0300

    refactor: use common function isRoleManaged to filter the data

--- apps/web/src/app/[lang]/app/[type]/management/[domain]/[data]/table-actions/permissions.tsx
+++ apps/web/src/app/[lang]/app/[type]/management/[domain]/[data]/table-actions/permissions.tsx
@@ -40,52 +40,40 @@ export default function PermissionsComponent({
   const [permissionsData, setPermissionsData] = useState<
     NormalizedPermissionGroup[]
   >([]);
-  const [updatedPermissions, setUpdatedPermissions] = useState<
+  const [updatedPermissionsData, setUpdatedPermissionsData] = useState<
     Volo_Abp_PermissionManagement_UpdatePermissionDto[]
   >([]);
   const [loadingError, setLoadingError] = useState(false);
   const isUserPage = params.data === "user";
+  const providerKey = isUserPage ? rowId : roleName;
+  const providerName = isUserPage ? "U" : "R";
 
   useEffect(() => {
-    async function fetchPermissions() {
-      const providerKey = isUserPage ? rowId : roleName;
-      const providerName = isUserPage ? "U" : "R";
-      const response = await getPermissionsApi({
-        providerKey,
-        providerName,
-      });
-
+    const fetchPermissions = async () => {
+      const response = await getPermissionsApi({ providerKey, providerName });
       if (response.type === "success") {
-        const normalizedGroups: NormalizedPermissionGroup[] =
+        const normalizedGroups =
           response.data.groups?.map((group) => ({
             ...group,
             permissions: group.permissions ?? [],
           })) || [];
         setPermissionsData(normalizedGroups);
-        setLoadingError(false);
       } else {
         setLoadingError(true);
         toast.error(
-          `${response.status}: ${
-            response.message || languageData["Permissions.Get.Fail"]
-          }`,
+          `${response.status}: ${response.message || languageData["Permissions.Get.Fail"]}`,
         );
       }
-    }
+    };
     void fetchPermissions();
   }, [rowId, roleName, isUserPage, params.lang]);
 
   const handleUpdatePermissions = async () => {
-    const providerKey = isUserPage ? rowId : roleName;
-    const providerName = isUserPage ? "U" : "R";
     const response = await putPermissionsApi({
       providerKey,
       providerName,
-      requestBody: {
-        permissions: updatedPermissions,
-      },
+      requestBody: { permissions: updatedPermissionsData },
     });
-
     if (response.type === "success") {
       toast.success(
         response.message || languageData["Permissions.Update.Success"],
@@ -93,265 +81,172 @@ export default function PermissionsComponent({
       window.location.reload();
     } else {
       toast.error(
-        `${response.status}: ${
-          response.message || languageData["Permissions.Update.Fail"]
-        }`,
+        `${response.status}: ${response.message || languageData["Permissions.Update.Fail"]}`,
       );
     }
   };
 
-  const updateChangedPermission = (
-    permissionName: string,
-    isGranted: boolean,
-  ) => {
-    setUpdatedPermissions((prevPermissions) => {
-      const existingPermissionIndex = prevPermissions.findIndex(
-        (p) => p.name === permissionName,
-      );
-      if (existingPermissionIndex >= 0) {
-        const updatedPermissionsList = [...prevPermissions];
-        updatedPermissionsList[existingPermissionIndex] = {
-          name: permissionName,
-          isGranted,
-        };
-        return updatedPermissionsList;
-      }
-      return [...prevPermissions, { name: permissionName, isGranted }];
-    });
-  };
-
-  const togglePermission = useCallback(
-    (groupName: string, permissionName: string) => {
-      setPermissionsData((prevData) =>
-        prevData.map((group) => {
-          if (group.name === groupName) {
-            return {
-              ...group,
-              permissions: group.permissions.map((permission) => {
-                const hasRoleProvider = permission.grantedProviders?.some(
-                  (provider) => provider.providerName === "R",
-                );
-
-                if (isUserPage && hasRoleProvider) return permission;
-
-                if (permission.name === permissionName) {
-                  const updatedPermission = {
-                    ...permission,
-                    isGranted: !permission.isGranted,
-                  };
-                  updateChangedPermission(
-                    permissionName,
-                    updatedPermission.isGranted,
-                  );
-                  if (!updatedPermission.isGranted) {
-                    updateChildPermissions(groupName, permissionName, false);
-                  } else {
-                    updateParentPermissions(
-                      groupName,
-                      permission.parentName || "",
-                      true,
-                    );
-                  }
-                  return updatedPermission;
-                }
-                return permission;
-              }),
-            };
-          }
-          return group;
-        }),
+  const isRoleManaged = useCallback(
+    (permission: Volo_Abp_PermissionManagement_PermissionGrantInfoDto) => {
+      return (
+        isUserPage &&
+        permission.grantedProviders?.some(
+          (provider) => provider.providerName === "R",
+        )
       );
     },
     [isUserPage],
   );
 
-  const updateChildPermissions = (
-    groupName: string,
-    parentName: string,
-    isGranted: boolean,
-  ) => {
-    setPermissionsData((prevData) =>
-      prevData.map((group) => {
-        if (group.name === groupName) {
-          return {
-            ...group,
-            permissions: group.permissions.map((permission) => {
-              if (
-                permission.parentName === parentName &&
-                (!isUserPage ||
-                  !permission.grantedProviders?.some(
-                    (provider) => provider.providerName === "R",
-                  ))
-              ) {
-                updateChangedPermission(permission.name || "", isGranted);
-                return {
-                  ...permission,
-                  isGranted,
-                };
-              }
-              return permission;
-            }),
-          };
-        }
-        return group;
-      }),
-    );
+  const permissionChange = (permissionName: string, isGranted: boolean) => {
+    setUpdatedPermissionsData((prev) => {
+      const updated = [...prev];
+      const existingIndex = updated.findIndex((p) => p.name === permissionName);
+      if (existingIndex >= 0) {
+        updated[existingIndex].isGranted = isGranted;
+      } else {
+        updated.push({ name: permissionName, isGranted });
+      }
+      return updated;
+    });
   };
 
-  const updateParentPermissions = (
-    groupName: string,
-    parentName: string | null,
-    isGranted: boolean,
-  ) => {
-    if (!parentName) return;
-    setPermissionsData((prevData) =>
-      prevData.map((group) => {
-        if (group.name === groupName) {
-          return {
-            ...group,
-            permissions: group.permissions.map((permission) => {
-              if (
-                permission.name === parentName &&
-                (!isUserPage ||
-                  !permission.grantedProviders?.some(
-                    (provider) => provider.providerName === "R",
-                  ))
-              ) {
-                const updatedPermission = {
-                  ...permission,
-                  isGranted,
-                };
-                updateChangedPermission(
-                  parentName,
-                  updatedPermission.isGranted,
-                );
-                updateParentPermissions(
+  const togglePermission = useCallback(
+    (groupName: string, permissionName: string) => {
+      setPermissionsData((prev) =>
+        prev.map((group) => {
+          if (group.name !== groupName) return group;
+
+          const updatedPermissions = group.permissions.map((permission) => {
+            if (
+              permission.name === permissionName &&
+              !isRoleManaged(permission)
+            ) {
+              const newGrant = !permission.isGranted;
+              permissionChange(permissionName, newGrant);
+              if (newGrant) {
+                permissionState(
                   groupName,
                   permission.parentName || "",
-                  isGranted,
+                  true,
+                  "parent",
                 );
-                return updatedPermission;
+              } else {
+                permissionState(groupName, permissionName, false, "child");
               }
-              return permission;
-            }),
-          };
-        }
-        return group;
+              return { ...permission, isGranted: newGrant };
+            }
+            return permission;
+          });
+          return { ...group, permissions: updatedPermissions };
+        }),
+      );
+    },
+    [isRoleManaged],
+  );
+
+  const permissionState = (
+    groupName: string,
+    permissionName: string,
+    isGranted: boolean,
+    type: "parent" | "child",
+  ) => {
+    setPermissionsData((prev) =>
+      prev.map((group) => {
+        if (group.name !== groupName) return group;
+        const updatedPermissions = group.permissions.map((permission) => {
+          const relevant =
+            type === "child"
+              ? permission.parentName === permissionName
+              : permission.name === permissionName;
+          if (relevant && !isRoleManaged(permission)) {
+            permissionChange(permission.name || "", isGranted);
+            if (type === "parent" && permission.parentName) {
+              permissionState(
+                groupName,
+                permission.parentName,
+                isGranted,
+                "parent",
+              );
+            }
+            if (type === "child") {
+              permissionState(
+                groupName,
+                permission.name || "",
+                isGranted,
+                "child",
+              );
+            }
+            return { ...permission, isGranted };
+          }
+          return permission;
+        });
+        return { ...group, permissions: updatedPermissions };
       }),
     );
   };
 
   const toggleGroupPermissions = (groupName: string, isGranted: boolean) => {
-    setPermissionsData((prevData) =>
-      prevData.map((group) => {
-        if (group.name === groupName) {
-          return {
-            ...group,
-            permissions: group.permissions.map((permission) => {
-              const hasRoleProvider = permission.grantedProviders?.some(
-                (provider) => provider.providerName === "R",
-              );
-              if (isUserPage && hasRoleProvider) {
-                return permission;
-              }
-              return {
-                ...permission,
-                isGranted,
-              };
-            }),
-          };
-        }
-        return group;
+    setPermissionsData((prev) =>
+      prev.map((group) => {
+        if (group.name !== groupName) return group;
+        const updatedPermissions = group.permissions.map((permission) => {
+          if (!isRoleManaged(permission)) {
+            permissionChange(permission.name || "", isGranted);
+            return { ...permission, isGranted };
+          }
+          return permission;
+        });
+        return { ...group, permissions: updatedPermissions };
       }),
     );
-    const groupPermissions = permissionsData.find(
-      (group) => group.name === groupName,
-    );
-    if (groupPermissions) {
-      groupPermissions.permissions.forEach((permission) => {
-        const hasRoleProvider = permission.grantedProviders?.some(
-          (provider) => provider.providerName === "R",
-        );
-        if (!isUserPage || !hasRoleProvider) {
-          updateChangedPermission(permission.name || "", isGranted);
-        }
-      });
-    }
   };
 
   const toggleAllPermissions = (isGranted: boolean) => {
-    setPermissionsData((prevData) =>
-      prevData.map((group) => ({
+    setPermissionsData((prev) =>
+      prev.map((group) => ({
         ...group,
         permissions: group.permissions.map((permission) => {
-          const hasRoleProvider = permission.grantedProviders?.some(
-            (provider) => provider.providerName === "R",
-          );
-          if (isUserPage && hasRoleProvider) {
-            return permission;
+          if (!isRoleManaged(permission)) {
+            permissionChange(permission.name || "", isGranted);
+            return { ...permission, isGranted };
           }
-          return {
-            ...permission,
-            isGranted,
-          };
+          return permission;
         }),
       })),
     );
-    setUpdatedPermissions(() => {
-      const allPermissions = permissionsData.flatMap((group) =>
-        group.permissions
-          .filter((permission) => {
-            const hasRoleProvider = permission.grantedProviders?.some(
-              (provider) => provider.providerName === "R",
-            );
-            return !isUserPage || !hasRoleProvider;
-          })
-          .map((permission) => ({
-            name: permission.name || "",
-            isGranted,
-          })),
-      );
-      return allPermissions;
-    });
   };
 
   const renderPermissions = useCallback(
     (groupName: string, parentName: string | null) => {
-      const group = permissionsData.find(
-        (currentGroup) => currentGroup.name === groupName,
-      );
+      const group = permissionsData.find((g) => g.name === groupName);
       if (!group) return null;
-
       const permissions = group.permissions.filter(
-        (permission) => permission.parentName === parentName,
+        (p) => p.parentName === parentName,
       );
-
       return (
-        <div className={parentName ? "ml-8" : "ml-8"}>
+        <div className={parentName ? "ml-8" : ""}>
           {permissions.map((permission) => {
-            const hasRoleProvider = permission.grantedProviders?.some(
-              (provider) => provider.providerName === "R",
-            );
-
+            const disabled = isRoleManaged(permission);
             return (
               <div className="mb-2 gap-2" key={permission.name}>
                 <Checkbox
                   checked={permission.isGranted || false}
                   className="mr-2"
-                  disabled={isUserPage ? hasRoleProvider : undefined}
+                  disabled={disabled}
                   onCheckedChange={() => {
                     togglePermission(groupName, permission.name || "");
                   }}
                 />
                 <span>{permission.displayName}</span>
-                {renderPermissions(groupName, permission.name || "")}
+                {renderPermissions(groupName, permission.name || null)}
               </div>
             );
           })}
         </div>
       );
     },
-    [permissionsData, togglePermission, isUserPage],
+    [permissionsData, togglePermission, isRoleManaged],
   );
 
   if (!permissionsData.length) {
@@ -367,68 +262,42 @@ export default function PermissionsComponent({
           checked={permissionsData.every((group) =>
             group.permissions.every((p) => p.isGranted),
           )}
-          disabled={
-            isUserPage
-              ? permissionsData.every((group) =>
-                  group.permissions.every((p) =>
-                    p.grantedProviders?.some(
-                      (provider) => provider.providerName === "R",
-                    ),
-                  ),
-                )
-              : undefined
-          }
+          disabled={permissionsData.every((group) =>
+            group.permissions.every((p) => isRoleManaged(p)),
+          )}
           onCheckedChange={(checked) => {
             toggleAllPermissions(checked === true);
           }}
         />
-        <span className="text-sm font-medium">
-          {languageData["Grant.All.Permissions"]}
-        </span>
+        <span>{languageData["Grant.All.Permissions"]}</span>
       </div>
 
       <SectionLayout
         sections={permissionsData.map((group) => ({
-          name: `${group.displayName} (${
-            group.permissions.filter((p) => p.isGranted).length
-          })`,
+          name: `${group.displayName} (${group.permissions.filter((p) => p.isGranted).length})`,
           id: group.name || "",
         }))}
         vertical
       >
         {permissionsData.map((group) => (
           <SectionLayoutContent key={group.name} sectionId={group.name || ""}>
-            <div className="flex items-center gap-2 ">
+            <div className="flex items-center gap-2">
               <Checkbox
                 checked={group.permissions.every((p) => p.isGranted)}
-                disabled={
-                  isUserPage
-                    ? group.permissions.every((p) =>
-                        p.grantedProviders?.some(
-                          (provider) => provider.providerName === "R",
-                        ),
-                      )
-                    : undefined
-                }
+                disabled={group.permissions.every((p) => isRoleManaged(p))}
                 onCheckedChange={(checked) => {
                   toggleGroupPermissions(group.name || "", checked === true);
                 }}
               />
-              <span className="text-sm font-medium">
-                {languageData["Select.All"]}
-              </span>
+              <span>{languageData["Select.All"]}</span>
             </div>
             <div className="my-2 border-t border-gray-200" />
             {renderPermissions(group.name || "", null)}
           </SectionLayoutContent>
         ))}
       </SectionLayout>
-
       <div className="bottom-0 left-0 flex w-full justify-end bg-white p-4 shadow-md">
-        <Button
-          onClick={() => void handleUpdatePermissions()}
-          variant="default"
-        >
+        <Button onClick={() => void handleUpdatePermissions()}>
           {languageData["Edit.Save"]}
         </Button>
       </div>
