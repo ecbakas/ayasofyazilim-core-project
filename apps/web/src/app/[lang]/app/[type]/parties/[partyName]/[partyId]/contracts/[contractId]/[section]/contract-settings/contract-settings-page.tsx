10902fa0484a0a3153d48eb5b5e45d1d11d828d0
Ertuğrulcan Bakas <ertugrul.bakas@ayasofyazilim.com>
2024-12-03 15:29:52 +0300

    feat(Contract): add page policies

--- apps/web/src/app/[lang]/app/[type]/parties/[partyName]/[partyId]/contracts/[contractId]/[section]/contract-settings/contract-settings-page.tsx
+++ apps/web/src/app/[lang]/app/[type]/parties/[partyName]/[partyId]/contracts/[contractId]/[section]/contract-settings/contract-settings-page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import type { UniRefund_LocationService_AddressCommonDatas_AddressCommonDataDto as AddressCommonDataDto } from "@ayasofyazilim/saas/LocationService";
 import { getResourceData } from "src/language-data/ContractService";
 import { getMerchantContractHeaderContractSettingsByHeaderIdApi } from "src/app/[lang]/app/actions/ContractService/action";
+import PagePolicy from "src/app/[lang]/page-policy/page-policy";
 import { ContractSettings } from "./contract-settings";
 
 export default async function ContractSettingsPage({
@@ -23,12 +24,14 @@ export default async function ContractSettingsPage({
     });
   if (contractSettings.type !== "success") return notFound();
   return (
-    <ContractSettings
-      addressList={addressList}
-      contractHeaderDetails={contractHeaderDetails}
-      contractSettings={contractSettings.data}
-      lang={lang}
-      languageData={languageData}
-    />
+    <PagePolicy requiredPolicies={["ContractService.ContractSetting.Edit"]}>
+      <ContractSettings
+        addressList={addressList}
+        contractHeaderDetails={contractHeaderDetails}
+        contractSettings={contractSettings.data}
+        lang={lang}
+        languageData={languageData}
+      />
+    </PagePolicy>
   );
 }
--- apps/web/src/app/[lang]/app/[type]/parties/[partyName]/[partyId]/contracts/[contractId]/[section]/contract/contract-header.tsx
+++ apps/web/src/app/[lang]/app/[type]/parties/[partyName]/[partyId]/contracts/[contractId]/[section]/contract/contract-header.tsx
@@ -6,6 +6,7 @@ import type {
 } from "@ayasofyazilim/saas/ContractService";
 import type { UniRefund_LocationService_AddressCommonDatas_AddressCommonDataDto as AddressCommonDataDto } from "@ayasofyazilim/saas/LocationService";
 import type { ContractServiceResource } from "src/language-data/ContractService";
+import PagePolicy from "src/app/[lang]/page-policy/page-policy";
 import ContractHeaderForm from "../../../contract-header-form";
 
 export function ContractHeader({
@@ -24,19 +25,23 @@ export function ContractHeader({
   languageData: ContractServiceResource;
 }) {
   return (
-    <ContractHeaderForm
-      addresses={addressList}
-      contractId={contractHeaderDetails.id}
-      formData={{
-        ...contractHeaderDetails,
-        status: contractHeaderDetails.status || "None",
-        addressCommonDataId: contractHeaderDetails.addressCommonData.id,
-      }}
-      formType="update"
-      languageData={languageData}
-      partyId={partyId}
-      partyName={partyName}
-      refundTableHeaders={refundTableHeaders}
-    />
+    <PagePolicy
+      requiredPolicies={["ContractService.ContractHeaderForMerchant.Edit"]}
+    >
+      <ContractHeaderForm
+        addresses={addressList}
+        contractId={contractHeaderDetails.id}
+        formData={{
+          ...contractHeaderDetails,
+          status: contractHeaderDetails.status || "None",
+          addressCommonDataId: contractHeaderDetails.addressCommonData.id,
+        }}
+        formType="update"
+        languageData={languageData}
+        partyId={partyId}
+        partyName={partyName}
+        refundTableHeaders={refundTableHeaders}
+      />
+    </PagePolicy>
   );
 }
