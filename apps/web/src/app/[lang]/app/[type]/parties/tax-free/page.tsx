c6a0581e21fa7708e20e73f71ba04623ccf20d64
Eren <ahmeterenodaci@gmail.com>
2024-12-10 11:04:49 +0300

    refactor(parteis): use new table and permission checks for action

---
+++ apps/web/src/app/[lang]/app/[type]/parties/tax-free/page.tsx
@@ -0,0 +1,36 @@
+"use server";
+
+import type { GetApiCrmServiceTaxFreesData } from "@ayasofyazilim/saas/CRMService";
+import { notFound } from "next/navigation";
+import PagePolicy from "src/app/[lang]/page-policy/page-policy";
+import { getResourceData } from "src/language-data/CRMService";
+import { getTaxFreesApi } from "../../../actions/CrmService/actions";
+import TaxFreeTable from "./table";
+
+interface SearchParamType {
+  maxResultCount?: number;
+  skipCount?: number;
+  sorting?: string;
+  name?: string;
+}
+
+export default async function Page(props: {
+  params: { lang: string };
+  searchParams?: Promise<SearchParamType>;
+}) {
+  const searchParams = await props.searchParams;
+  const response = await getTaxFreesApi({
+    name: searchParams?.name || "",
+    maxResultCount: searchParams?.maxResultCount || 10,
+    skipCount: searchParams?.skipCount || 0,
+  } as GetApiCrmServiceTaxFreesData);
+
+  if (response.type !== "success") return notFound();
+
+  const { languageData } = await getResourceData(props.params.lang);
+  return (
+    <PagePolicy requiredPolicies={["CRMService.TaxFrees"]}>
+      <TaxFreeTable languageData={languageData} response={response.data} />
+    </PagePolicy>
+  );
+}
---
+++ apps/web/src/app/[lang]/app/[type]/parties/tax-offices/page.tsx
@@ -0,0 +1,36 @@
+"use server";
+
+import type { GetApiCrmServiceTaxOfficesData } from "@ayasofyazilim/saas/CRMService";
+import { notFound } from "next/navigation";
+import PagePolicy from "src/app/[lang]/page-policy/page-policy";
+import { getResourceData } from "src/language-data/CRMService";
+import { getTaxOfficesApi } from "../../../actions/CrmService/actions";
+import TaxOfficesTable from "./table";
+
+interface SearchParamType {
+  maxResultCount?: number;
+  skipCount?: number;
+  sorting?: string;
+  name?: string;
+}
+
+export default async function Page(props: {
+  params: { lang: string };
+  searchParams?: Promise<SearchParamType>;
+}) {
+  const searchParams = await props.searchParams;
+  const response = await getTaxOfficesApi({
+    name: searchParams?.name || "",
+    maxResultCount: searchParams?.maxResultCount || 10,
+    skipCount: searchParams?.skipCount || 0,
+  } as GetApiCrmServiceTaxOfficesData);
+
+  if (response.type !== "success") return notFound();
+
+  const { languageData } = await getResourceData(props.params.lang);
+  return (
+    <PagePolicy requiredPolicies={["CRMService.TaxOffices"]}>
+      <TaxOfficesTable languageData={languageData} response={response.data} />
+    </PagePolicy>
+  );
+}
