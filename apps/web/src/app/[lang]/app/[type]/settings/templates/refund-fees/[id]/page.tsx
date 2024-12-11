15a4f478ecd685d4d3b65eb4c02cf7ab2097f08f
Eren <ahmeterenodaci@gmail.com>
2024-12-11 09:57:22 +0300

    chore(contract/refundfees): permission check

--- apps/web/src/app/[lang]/app/[type]/settings/templates/refund-fees/[id]/page.tsx
+++ apps/web/src/app/[lang]/app/[type]/settings/templates/refund-fees/[id]/page.tsx
@@ -1,6 +1,8 @@
+"use server";
 import { notFound } from "next/navigation";
 import { getRefundFeeHeadersByIdApi } from "src/app/[lang]/app/actions/ContractService/action";
 import { getResourceData } from "src/language-data/ContractService";
+import PagePolicy from "src/app/[lang]/page-policy/page-policy";
 import Form from "./form";
 import RefundFeeDetailsForm from "./table";
 
@@ -8,25 +10,35 @@ export default async function Page({
   params,
 }: {
   params: { lang: string; id: string };
-}): Promise<JSX.Element> {
+}) {
   const response = await getRefundFeeHeadersByIdApi({ id: params.id });
   if (response.type !== "success") return notFound();
 
   const { languageData } = await getResourceData(params.lang);
-
   return (
-    <>
-      <Form languageData={languageData} response={response.data} />
-      <RefundFeeDetailsForm
-        languageData={languageData}
-        response={response.data}
-      />
-      <div className="hidden" id="page-title">
-        {response.data.name}
-      </div>
-      <div className="hidden" id="page-description">
-        {languageData["RefundTables.Edit.Description"]}
-      </div>
-    </>
+    <PagePolicy
+      requiredPolicies={[
+        "ContractService.RefundFeeDetail.Create",
+        "ContractService.RefundFeeDetail.Edit",
+        "ContractService.RefundFeeDetail.Delete",
+        "ContractService.RefundFeeHeader.Create",
+        "ContractService.RefundFeeHeader.Edit",
+        "ContractService.RefundFeeHeader.Delete",
+      ]}
+    >
+      <>
+        <Form languageData={languageData} response={response.data} />
+        <RefundFeeDetailsForm
+          languageData={languageData}
+          response={response.data}
+        />
+        <div className="hidden" id="page-title">
+          {response.data.name}
+        </div>
+        <div className="hidden" id="page-description">
+          {languageData["RefundTables.Edit.Description"]}
+        </div>
+      </>
+    </PagePolicy>
   );
 }
