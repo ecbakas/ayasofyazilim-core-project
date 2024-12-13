20f5c21478f07c05dc23596e493482e2517d36e3
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-12-13 10:24:59 +0300

    chore:clear language data

--- apps/web/src/language-data/language-data.ts
+++ apps/web/src/language-data/language-data.ts
@@ -1,11 +1,8 @@
 import type { AbpUiNavigationResource } from "./AbpUiNavigation";
 import type { ContractServiceResource } from "./ContractService";
 import type { CRMServiceServiceResource } from "./CRMService";
-import type { CustomerServiceResource } from "./CustomersService";
-import type { DebtorsServiceResource } from "./DebtorsService";
 import type { DefaultResource } from "./Default";
 import type { IdentityServiceResource } from "./IdentityService";
-import type { ProjectServiceResource } from "./ProjectService";
 import type { SettingServiceResource } from "./SettingService";
 import type { TravellerServiceResource } from "./TravellerService";
 
@@ -14,10 +11,7 @@ export type LanguageDataResourceType = DefaultResource &
     | AbpUiNavigationResource
     | ContractServiceResource
     | CRMServiceServiceResource
-    | CustomerServiceResource
-    | DebtorsServiceResource
     | IdentityServiceResource
-    | ProjectServiceResource
     | SettingServiceResource
     | TravellerServiceResource
     | undefined
