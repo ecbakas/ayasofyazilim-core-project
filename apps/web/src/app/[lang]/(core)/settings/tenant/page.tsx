9c13705b4f209f1c4a995d82eeb6890e0337f3e3
yusualhashash <yusufalhashash2018@gmail.com>
2024-12-16 11:23:09 +0300

    chore: remove unused code

--- apps/web/src/app/[lang]/(core)/settings/tenant/page.tsx
+++ apps/web/src/app/[lang]/(core)/settings/tenant/page.tsx
@@ -1,11 +1,7 @@
 import { RedirectType, permanentRedirect } from "next/navigation";
 import { getBaseLink } from "src/utils";
 
-export default function Page({
-  params,
-}: {
-  params: { lang: string; type: string };
-}) {
+export default function Page({ params }: { params: { lang: string } }) {
   permanentRedirect(
     getBaseLink(`/settings/tenant/home`, true, params.lang),
     RedirectType.push,
