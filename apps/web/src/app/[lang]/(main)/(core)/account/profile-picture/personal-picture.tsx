ae588c98f85b786f8ef20e6f3eacfe253f770927
yusualhashash <yusufalhashash2018@gmail.com>
2024-12-23 15:27:38 +0300

    feat: add personal picture of profile list

---
+++ apps/web/src/app/[lang]/(main)/(core)/account/profile-picture/personal-picture.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import type { Volo_Abp_Account_ProfilePictureType } from "@ayasofyazilim/saas/AccountService";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { postProfilePictureApi } from "src/actions/core/AccountService/actions";
+import { handlePutResponse } from "src/actions/core/api-utils-client";
+import type { AccountServiceResource } from "src/language-data/core/AccountService";
+
+export default function PersonalPicture({
+  languageData,
+}: {
+  languageData: AccountServiceResource;
+}) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<Blob | File | undefined>(
+    undefined,
+  );
+  const [uploadType, setUploadType] =
+    useState<Volo_Abp_Account_ProfilePictureType>(2);
+
+  const handleSaveImage = () => {
+    setLoading(true);
+    void postProfilePictureApi({
+      formData: {
+        ImageContent: selectedImage,
+      },
+      type: uploadType,
+    })
+      .then((response) => {
+        handlePutResponse(response, router);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-6">
+      <div className="flex h-64 w-64 items-center justify-center overflow-hidden rounded-lg border border-gray-300 bg-gray-100">
+        {selectedImage ? (
+          <></>
+        ) : (
+          // <img
+          // src={selectedImage}
+          // alt="Selected Profile Picture"
+          // className="h-full w-full object-cover"
+          // />
+          <span className="text-gray-500">
+            {languageData["No.Picture.Selected"]}
+          </span>
+        )}
+      </div>
+      <form>
+        <div className="flex flex-col items-start gap-4 px-96 pl-64 pt-4">
+          <label className="flex items-center gap-2">
+            <input
+              checked={uploadType === 0}
+              name="uploadType"
+              onChange={() => {
+                setUploadType(0);
+                setSelectedImage(undefined);
+              }}
+              type="radio"
+              value={0}
+            />
+            {languageData["Default.Avatar"]}
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              checked={uploadType === 1}
+              name="uploadType"
+              onChange={() => {
+                setUploadType(1);
+                setSelectedImage(undefined);
+              }}
+              type="radio"
+              value={1}
+            />
+            {languageData["Use.Gravatar"]}
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              checked={uploadType === 2}
+              name="uploadType"
+              onChange={() => {
+                setUploadType(2);
+              }}
+              type="radio"
+              value={2}
+            />
+            {languageData["Upload.File"]}
+          </label>
+        </div>
+
+        {uploadType === 2 && (
+          <div>
+            <input
+              className="block w-full cursor-pointer rounded-lg border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 hover:bg-gray-100 focus:outline-none"
+              id="file"
+              type="file"
+            />
+          </div>
+        )}
+        <div className="flex justify-end pr-72 pt-8">
+          <Button
+            disabled={loading}
+            onClick={() => {
+              handleSaveImage();
+            }}
+          >
+            {languageData["Edit.Save"]}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
