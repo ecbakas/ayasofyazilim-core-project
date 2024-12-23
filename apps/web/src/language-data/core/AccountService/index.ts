0b4f646e93443565df388e970388be9955203dd6
yusualhashash <yusufalhashash2018@gmail.com>
2024-12-23 15:28:58 +0300

    chore: add translation

---
+++ apps/web/src/language-data/core/AccountService/index.ts
@@ -0,0 +1,31 @@
+import { getLocalizationResources } from "src/utils";
+import defaultEn from "../Default/resources/en.json";
+import defaultTr from "../Default/resources/tr.json";
+import en from "./resources/en.json";
+import tr from "./resources/tr.json";
+
+export type AccountServiceResource = typeof en & typeof defaultEn;
+function getLanguageData(lang: string): AccountServiceResource {
+  if (lang === "tr") {
+    return {
+      ...defaultTr,
+      ...tr,
+    };
+  }
+  return {
+    ...defaultEn,
+    ...en,
+  };
+}
+export async function getResourceData(lang: string) {
+  const resources = await getLocalizationResources(lang);
+  const languageData = getLanguageData(lang);
+  return {
+    languageData,
+    resources,
+  };
+}
+export function getResourceDataClient(lang: string) {
+  const languageData = getLanguageData(lang);
+  return languageData;
+}
--- apps/web/src/utils/navbar/navbar-profile-data.tsx
+++ apps/web/src/utils/navbar/navbar-profile-data.tsx
@@ -1,5 +1,5 @@
 import type { ProfileMenuProps } from "@repo/ui/theme/types";
-import { Layers, Settings, User } from "lucide-react";
+import { Settings } from "lucide-react";
 import type { AbpUiNavigationResource } from "src/language-data/core/AbpUiNavigation";
 
 export function getProfileMenuFromDB(languageData: AbpUiNavigationResource) {
@@ -13,25 +13,19 @@ export function getProfileMenuFromDB(languageData: AbpUiNavigationResource) {
     menu: {
       account: [
         {
-          icon: <User className="mr-2 h-5 w-5 text-gray-400" />,
-          href: "#",
+          name: languageData.MyAccount,
+          href: "/account/sessions",
           onClick: undefined,
-          name: languageData.Profile,
-        },
-        {
-          href: "#",
-          onClick: undefined,
-          name: languageData.UserSettings,
           icon: <Settings className="mr-2 h-5 w-5 text-gray-400" />,
         },
       ],
       primary: [
-        {
-          href: "#",
-          onClick: undefined,
-          name: languageData.Management,
-          icon: <Layers className="mr-2 h-5 w-5 text-gray-400" />,
-        },
+        // {
+        //   href: "#",
+        //   onClick: undefined,
+        //   name: languageData.Management,
+        //   icon: <Layers className="mr-2 h-5 w-5 text-gray-400" />,
+        // },
       ],
       secondary: [],
     },
