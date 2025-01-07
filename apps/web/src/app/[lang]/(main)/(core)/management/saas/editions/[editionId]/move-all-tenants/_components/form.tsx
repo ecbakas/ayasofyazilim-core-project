ce5e196b80f744b9cc8daebdb0ee2877061cd454
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-07 23:35:08 +0300

    feat: add move all tenant action to edition page

---
+++ apps/web/src/app/[lang]/(main)/(core)/management/saas/editions/[editionId]/move-all-tenants/_components/form.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import type { Volo_Saas_Host_Dtos_EditionDto } from "@ayasofyazilim/saas/SaasService";
+import { SchemaForm } from "@repo/ayasofyazilim-ui/organisms/schema-form";
+import { createUiSchemaWithResource } from "@repo/ayasofyazilim-ui/organisms/schema-form/utils";
+import { CustomComboboxWidget } from "@repo/ayasofyazilim-ui/organisms/schema-form/widgets";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { handlePutResponse } from "src/actions/core/api-utils-client";
+import { putEditionsByIdMoveAllTenantsApi } from "src/actions/core/SaasService/put-actions";
+import type { SaasServiceResource } from "src/language-data/core/SaasService";
+
+interface EditionParams {
+  editionId: string;
+}
+
+const $Volo_Abp_Identity_UpdateMoveAllTenantsDto = {
+  type: "object",
+  required: ["editionId"],
+  properties: {
+    editionId: {
+      type: "string",
+      maxLength: 256,
+      minLength: 0,
+    },
+  },
+};
+
+export default function Form({
+  languageData,
+  editionList,
+}: {
+  languageData: SaasServiceResource;
+  editionList: Volo_Saas_Host_Dtos_EditionDto[];
+}) {
+  const router = useRouter();
+  const { editionId } = useParams<{ editionId: string }>();
+  const [loading, setLoading] = useState(false);
+  const uiSchema = createUiSchemaWithResource({
+    schema: $Volo_Abp_Identity_UpdateMoveAllTenantsDto,
+    resources: languageData,
+    name: "Form.Edition",
+    extend: {
+      editionId: {
+        "ui:widget": "EditionWidget",
+      },
+    },
+  });
+  return (
+    <SchemaForm<EditionParams>
+      className="flex flex-col gap-4"
+      disabled={loading}
+      onSubmit={(data) => {
+        setLoading(true);
+        const formData = data.formData;
+        if (!formData) return;
+        void putEditionsByIdMoveAllTenantsApi({
+          id: editionId,
+          editionId: formData.editionId,
+        })
+          .then((res) => {
+            handlePutResponse(res, router);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      }}
+      schema={$Volo_Abp_Identity_UpdateMoveAllTenantsDto}
+      submitText={languageData.Save}
+      uiSchema={uiSchema}
+      widgets={{
+        EditionWidget: CustomComboboxWidget<Volo_Saas_Host_Dtos_EditionDto>({
+          languageData,
+          list: editionList.filter((edition) => edition.id !== editionId),
+          selectIdentifier: "id",
+          selectLabel: "displayName",
+        }),
+      }}
+    />
+  );
+}
---
+++ apps/web/src/app/[lang]/(main)/(core)/management/saas/editions/[editionId]/move-all-tenants/page.tsx
@@ -0,0 +1,54 @@
+"use server";
+
+import {
+  getAllEditionsApi,
+  getEditionDetailsByIdApi,
+} from "src/actions/core/SaasService/actions";
+import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
+import { getResourceData } from "src/language-data/core/SaasService";
+import { isUnauthorized } from "src/utils/page-policy/page-policy";
+import { isErrorOnRequest } from "src/utils/page-policy/utils";
+import Form from "./_components/form";
+
+export default async function Page({
+  params,
+}: {
+  params: { lang: string; editionId: string };
+}) {
+  const { lang, editionId } = params;
+  const { languageData } = await getResourceData(lang);
+  await isUnauthorized({
+    requiredPolicies: ["Saas.Editions.Update"],
+    lang,
+  });
+
+  const editionsResponse = await getAllEditionsApi();
+  if (isErrorOnRequest(editionsResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={editionsResponse.message}
+      />
+    );
+  }
+  const editionDetailsResponse = await getEditionDetailsByIdApi(editionId);
+  if (isErrorOnRequest(editionDetailsResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={editionDetailsResponse.message}
+      />
+    );
+  }
+  return (
+    <>
+      <Form editionList={editionsResponse.data} languageData={languageData} />
+      <div className="hidden" id="page-title">
+        {`${languageData.Edition} (${editionDetailsResponse.data.displayName})`}
+      </div>
+      <div className="hidden" id="page-description">
+        {languageData["Edition.MoveAllTenants.Description"]}
+      </div>
+    </>
+  );
+}
