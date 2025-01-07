68893ea84292dd575e4c59e48fc08021194346b8
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-07 23:20:22 +0300

    feat: add create & edit forms

---
+++ apps/web/src/app/[lang]/(main)/(core)/management/saas/editions/[editionId]/_components/form.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import type { Volo_Saas_Host_Dtos_EditionDto } from "@ayasofyazilim/saas/SaasService";
+import { $Volo_Saas_Host_Dtos_EditionUpdateDto } from "@ayasofyazilim/saas/SaasService";
+import { ActionList } from "@repo/ayasofyazilim-ui/molecules/action-button";
+import ConfirmDialog from "@repo/ayasofyazilim-ui/molecules/confirm-dialog";
+import { SchemaForm } from "@repo/ayasofyazilim-ui/organisms/schema-form";
+import { createUiSchemaWithResource } from "@repo/ayasofyazilim-ui/organisms/schema-form/utils";
+import { Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import {
+  handleDeleteResponse,
+  handlePutResponse,
+} from "src/actions/core/api-utils-client";
+import { deleteEditionByIdApi } from "src/actions/core/SaasService/delete-actions";
+import { putEditionApi } from "src/actions/core/SaasService/put-actions";
+import type { SaasServiceResource } from "src/language-data/core/SaasService";
+import { useGrantedPolicies } from "src/providers/granted-policies";
+import isActionGranted from "src/utils/page-policy/action-policy";
+
+export default function Form({
+  languageData,
+  response,
+}: {
+  languageData: SaasServiceResource;
+  response: Volo_Saas_Host_Dtos_EditionDto;
+}) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { grantedPolicies } = useGrantedPolicies();
+
+  const uiSchema = createUiSchemaWithResource({
+    schema: $Volo_Saas_Host_Dtos_EditionUpdateDto,
+    resources: languageData,
+    name: "Form.Edition",
+  });
+  return (
+    <div className="flex flex-col gap-4 overflow-auto">
+      <ActionList>
+        {isActionGranted(["Saas.Editions.Delete"], grantedPolicies) && (
+          <ConfirmDialog
+            closeProps={{
+              children: languageData.Cancel,
+            }}
+            confirmProps={{
+              variant: "destructive",
+              children: languageData.Delete,
+              onConfirm: () => {
+                setLoading(true);
+                void deleteEditionByIdApi(response.id || "")
+                  .then((res) => {
+                    handleDeleteResponse(res);
+                    if (res.type === "success") router.push(`../editions`);
+                  })
+                  .finally(() => {
+                    setLoading(false);
+                  });
+              },
+              closeAfterConfirm: true,
+            }}
+            description={languageData["Delete.Assurance"]}
+            title={languageData["Edition.Delete"]}
+            triggerProps={{
+              children: (
+                <>
+                  <Trash2 className="mr-2 w-4" /> {languageData.Delete}
+                </>
+              ),
+              variant: "outline",
+            }}
+            type="with-trigger"
+          />
+        )}
+      </ActionList>
+      <SchemaForm
+        className="flex flex-col gap-4"
+        disabled={loading}
+        filter={{
+          type: "exclude",
+          keys: ["planId", "concurrencyStamp"],
+        }}
+        formData={response}
+        onSubmit={(data) => {
+          setLoading(true);
+          const formData = data.formData;
+          void putEditionApi({
+            id: response.id || "",
+            requestBody: {
+              ...formData,
+              displayName: formData?.displayName || "",
+            },
+          })
+            .then((res) => {
+              handlePutResponse(res, router);
+            })
+            .finally(() => {
+              setLoading(false);
+            });
+        }}
+        schema={$Volo_Saas_Host_Dtos_EditionUpdateDto}
+        submitText={languageData["Edit.Save"]}
+        uiSchema={uiSchema}
+      />
+    </div>
+  );
+}
---
+++ apps/web/src/app/[lang]/(main)/(core)/management/saas/editions/[editionId]/page.tsx
@@ -0,0 +1,44 @@
+"use server";
+
+import { getEditionDetailsByIdApi } from "src/actions/core/SaasService/actions";
+import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
+import { getResourceData } from "src/language-data/core/SaasService";
+import { isUnauthorized } from "src/utils/page-policy/page-policy";
+import { isErrorOnRequest } from "src/utils/page-policy/utils";
+import Form from "./_components/form";
+
+export default async function Page({
+  params,
+}: {
+  params: { lang: string; editionId: string };
+}) {
+  const { lang, editionId } = params;
+  const { languageData } = await getResourceData(lang);
+  await isUnauthorized({
+    requiredPolicies: ["Saas.Editions.Update"],
+    lang,
+  });
+  const editionDetailsResponse = await getEditionDetailsByIdApi(editionId);
+  if (isErrorOnRequest(editionDetailsResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={editionDetailsResponse.message}
+      />
+    );
+  }
+  return (
+    <>
+      <Form
+        languageData={languageData}
+        response={editionDetailsResponse.data}
+      />
+      <div className="hidden" id="page-title">
+        {`${languageData.Edition} (${editionDetailsResponse.data.displayName})`}
+      </div>
+      <div className="hidden" id="page-description">
+        {languageData["Edition.Update.Description"]}
+      </div>
+    </>
+  );
+}
