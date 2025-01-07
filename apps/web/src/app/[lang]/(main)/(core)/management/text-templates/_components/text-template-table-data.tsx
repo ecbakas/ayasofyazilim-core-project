afd7b0cb6bf65752e59cfc5001a1606ae485ec9c
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-07 09:48:14 +0300

    feat(text template): add text template page

---
+++ apps/web/src/app/[lang]/(main)/(core)/management/text-templates/_components/text-template-table-data.tsx
@@ -0,0 +1,101 @@
+import type { Volo_Abp_TextTemplateManagement_TextTemplates_TemplateDefinitionDto } from "@ayasofyazilim/saas/AdministrationService";
+import { $Volo_Abp_TextTemplateManagement_TextTemplates_TemplateDefinitionDto } from "@ayasofyazilim/saas/AdministrationService";
+import type {
+  TanstackTableColumnLink,
+  TanstackTableCreationProps,
+} from "@repo/ayasofyazilim-ui/molecules/tanstack-table/types";
+import { tanstackTableCreateColumnsByRowData } from "@repo/ayasofyazilim-ui/molecules/tanstack-table/utils";
+import { CheckCircle, XCircle } from "lucide-react";
+import type { AdministrationServiceResource } from "src/language-data/core/AdministrationService";
+
+type TextTemplateTable =
+  TanstackTableCreationProps<Volo_Abp_TextTemplateManagement_TextTemplates_TemplateDefinitionDto>;
+
+const links: Partial<
+  Record<
+    keyof Volo_Abp_TextTemplateManagement_TextTemplates_TemplateDefinitionDto,
+    TanstackTableColumnLink
+  >
+> = {};
+
+const textTemplateColumns = (
+  locale: string,
+  languageData: AdministrationServiceResource,
+) => {
+  return tanstackTableCreateColumnsByRowData<Volo_Abp_TextTemplateManagement_TextTemplates_TemplateDefinitionDto>(
+    {
+      rows: $Volo_Abp_TextTemplateManagement_TextTemplates_TemplateDefinitionDto.properties,
+      languageData: {
+        languageData,
+        constantKey: "Form.TextTemplate",
+      },
+      config: {
+        locale,
+      },
+      links,
+      badges: {
+        displayName: {
+          values: [
+            {
+              position: "after",
+              label: languageData["Form.TextTemplate.isLayout"],
+              badgeClassName: "text-green-500 bg-green-100 border-green-500",
+              conditions: [
+                {
+                  when: (value) => value === true,
+                  conditionAccessorKey: "isLayout",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      faceted: {
+        isInlineLocalized: {
+          options: [
+            {
+              value: "true",
+              label: "",
+              icon: CheckCircle,
+              iconClassName: "text-green-700",
+            },
+            {
+              value: "false",
+              label: "",
+              icon: XCircle,
+              iconClassName: "text-red-700",
+            },
+          ],
+        },
+      },
+    },
+  );
+};
+
+function textTemplateTable() {
+  const table: TextTemplateTable = {
+    fillerColumn: "name",
+    pinColumns: ["name"],
+    filters: {
+      textFilters: ["filterText"],
+    },
+    columnVisibility: {
+      type: "hide",
+      columns: ["name", "isLayout"],
+    },
+    columnOrder: [
+      "displayName",
+      "isInlineLocalized",
+      "layout",
+      "defaultCultureName",
+    ],
+  };
+  return table;
+}
+
+export const tableData = {
+  textTemplate: {
+    columns: textTemplateColumns,
+    table: textTemplateTable,
+  },
+};
