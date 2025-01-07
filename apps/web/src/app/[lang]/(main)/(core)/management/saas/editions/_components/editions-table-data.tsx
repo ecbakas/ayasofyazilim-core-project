0672a0df7d3676b0348fae7e65e7773868bb4c32
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-07 23:18:21 +0300

    feat: add edition table

---
+++ apps/web/src/app/[lang]/(main)/(core)/management/saas/editions/_components/editions-table-data.tsx
@@ -0,0 +1,110 @@
+import type { Volo_Saas_Host_Dtos_EditionDto } from "@ayasofyazilim/saas/SaasService";
+import { $Volo_Saas_Host_Dtos_EditionDto } from "@ayasofyazilim/saas/SaasService";
+import type {
+  TanstackTableColumnLink,
+  TanstackTableCreationProps,
+  TanstackTableRowActionsType,
+  TanstackTableTableActionsType,
+} from "@repo/ayasofyazilim-ui/molecules/tanstack-table/types";
+import { tanstackTableCreateColumnsByRowData } from "@repo/ayasofyazilim-ui/molecules/tanstack-table/utils";
+import { Plus, User2Icon } from "lucide-react";
+import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import type { SaasServiceResource } from "src/language-data/core/SaasService";
+import isActionGranted from "src/utils/page-policy/action-policy";
+import type { Policy } from "src/utils/page-policy/utils";
+
+type EditionsTable = TanstackTableCreationProps<Volo_Saas_Host_Dtos_EditionDto>;
+
+const links: Partial<
+  Record<keyof Volo_Saas_Host_Dtos_EditionDto, TanstackTableColumnLink>
+> = {};
+
+function editionsTableActions(
+  languageData: SaasServiceResource,
+  router: AppRouterInstance,
+  grantedPolicies: Record<Policy, boolean>,
+) {
+  const actions: TanstackTableTableActionsType[] = [];
+  if (isActionGranted(["Saas.Editions.Create"], grantedPolicies)) {
+    actions.push({
+      type: "simple",
+      actionLocation: "table",
+      cta: languageData["Edition.New"],
+      icon: Plus,
+      onClick: () => {
+        router.push("editions/new");
+      },
+    });
+  }
+  return actions;
+}
+function editionsRowActions(
+  languageData: SaasServiceResource,
+  router: AppRouterInstance,
+  grantedPolicies: Record<Policy, boolean>,
+) {
+  const actions: TanstackTableRowActionsType<Volo_Saas_Host_Dtos_EditionDto>[] =
+    [];
+  if (isActionGranted(["Saas.Editions.Update"], grantedPolicies)) {
+    actions.push({
+      type: "simple",
+      actionLocation: "row",
+      cta: languageData["Edition.MoveAllTenants"],
+      icon: User2Icon,
+      onClick: (row) => {
+        router.push(`editions/${row.id}/move-all-tenants`);
+      },
+    });
+  }
+  return actions;
+}
+const editionsColumns = (
+  locale: string,
+  languageData: SaasServiceResource,
+  grantedPolicies: Record<Policy, boolean>,
+) => {
+  if (isActionGranted(["Saas.Editions.Update"], grantedPolicies)) {
+    links.displayName = {
+      prefix: "editions",
+      targetAccessorKey: "id",
+    };
+  }
+  return tanstackTableCreateColumnsByRowData<Volo_Saas_Host_Dtos_EditionDto>({
+    rows: $Volo_Saas_Host_Dtos_EditionDto.properties,
+    languageData: {
+      languageData,
+      constantKey: "Form.Edition",
+    },
+    config: {
+      locale,
+    },
+    links,
+  });
+};
+function editionsTable(
+  languageData: SaasServiceResource,
+  router: AppRouterInstance,
+  grantedPolicies: Record<Policy, boolean>,
+) {
+  const table: EditionsTable = {
+    fillerColumn: "displayName",
+    pinColumns: ["displayName"],
+    columnVisibility: {
+      type: "show",
+      columns: ["displayName", "tenantCount"],
+    },
+    filters: {
+      textFilters: ["filter"],
+    },
+    tableActions: editionsTableActions(languageData, router, grantedPolicies),
+    rowActions: editionsRowActions(languageData, router, grantedPolicies),
+  };
+  return table;
+}
+
+export const tableData = {
+  editions: {
+    columns: editionsColumns,
+    table: editionsTable,
+  },
+};
---
+++ apps/web/src/app/[lang]/(main)/(core)/management/saas/editions/_components/table.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import type { Volo_Abp_Application_Dtos_PagedResultDto_1 } from "@ayasofyazilim/saas/SaasService";
+import TanstackTable from "@repo/ayasofyazilim-ui/molecules/tanstack-table";
+import { useParams, useRouter } from "next/navigation";
+import type { SaasServiceResource } from "src/language-data/core/SaasService";
+import { useGrantedPolicies } from "src/providers/granted-policies";
+import { tableData } from "./editions-table-data";
+
+function EditionsTable({
+  response,
+  languageData,
+}: {
+  response: Volo_Abp_Application_Dtos_PagedResultDto_1;
+  languageData: SaasServiceResource;
+}) {
+  const router = useRouter();
+  const { lang } = useParams<{ lang: string }>();
+  const { grantedPolicies } = useGrantedPolicies();
+  const columns = tableData.editions.columns(
+    lang,
+    languageData,
+    grantedPolicies,
+  );
+  const table = tableData.editions.table(languageData, router, grantedPolicies);
+
+  return (
+    <TanstackTable
+      {...table}
+      columns={columns}
+      data={response.items || []}
+      rowCount={response.totalCount}
+    />
+  );
+}
+export default EditionsTable;
