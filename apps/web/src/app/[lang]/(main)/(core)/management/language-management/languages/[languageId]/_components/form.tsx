0fde77ad967305a94bcc7273352f97c69eca2a46
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-07 11:08:49 +0300

    feat: add create and edit forms

---
+++ apps/web/src/app/[lang]/(main)/(core)/management/language-management/languages/[languageId]/_components/form.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import type {
+  Volo_Abp_LanguageManagement_Dto_LanguageDto,
+  Volo_Abp_LanguageManagement_Dto_UpdateLanguageDto,
+} from "@ayasofyazilim/saas/AdministrationService";
+import { $Volo_Abp_LanguageManagement_Dto_UpdateLanguageDto } from "@ayasofyazilim/saas/AdministrationService";
+import { ActionList } from "@repo/ayasofyazilim-ui/molecules/action-button";
+import ConfirmDialog from "@repo/ayasofyazilim-ui/molecules/confirm-dialog";
+import { SchemaForm } from "@repo/ayasofyazilim-ui/organisms/schema-form";
+import { createUiSchemaWithResource } from "@repo/ayasofyazilim-ui/organisms/schema-form/utils";
+import { Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { deleteLanguageByIdApi } from "src/actions/core/AdministrationService/delete-actions";
+import { putLanguageApi } from "src/actions/core/AdministrationService/put-actions";
+import {
+  handleDeleteResponse,
+  handlePutResponse,
+} from "src/actions/core/api-utils-client";
+import type { AdministrationServiceResource } from "src/language-data/core/AdministrationService";
+import { useGrantedPolicies } from "src/providers/granted-policies";
+import isActionGranted from "src/utils/page-policy/action-policy";
+
+export default function Form({
+  languageData,
+  languageDetailsData,
+}: {
+  languageData: AdministrationServiceResource;
+  languageDetailsData: Volo_Abp_LanguageManagement_Dto_LanguageDto;
+}) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { grantedPolicies } = useGrantedPolicies();
+
+  const uiSchema = createUiSchemaWithResource({
+    schema: $Volo_Abp_LanguageManagement_Dto_UpdateLanguageDto,
+    resources: languageData,
+    name: "Form.Language",
+    extend: {
+      isEnabled: {
+        "ui:widget": "switch",
+      },
+    },
+  });
+  return (
+    <div className="flex flex-col gap-4 overflow-auto">
+      <ActionList>
+        {isActionGranted(
+          ["LanguageManagement.Languages.Delete"],
+          grantedPolicies,
+        ) && (
+          <ConfirmDialog
+            closeProps={{
+              children: languageData.Cancel,
+            }}
+            confirmProps={{
+              variant: "destructive",
+              children: languageData.Delete,
+              onConfirm: () => {
+                setLoading(true);
+                void deleteLanguageByIdApi(languageDetailsData.id || "")
+                  .then((res) => {
+                    handleDeleteResponse(res);
+                    if (res.type === "success") router.push(`../languages`);
+                  })
+                  .finally(() => {
+                    setLoading(false);
+                  });
+              },
+              closeAfterConfirm: true,
+            }}
+            description={languageData["Delete.Assurance"]}
+            title={languageData["Language.Delete"]}
+            triggerProps={{
+              children: (
+                <>
+                  <Trash2 className="mr-2 w-4" /> {languageData.Delete}
+                </>
+              ),
+              variant: "outline",
+            }}
+            type="with-trigger"
+          />
+        )}
+      </ActionList>
+      <SchemaForm<Volo_Abp_LanguageManagement_Dto_UpdateLanguageDto>
+        className="flex flex-col gap-4"
+        disabled={loading}
+        filter={{
+          type: "include",
+          sort: true,
+          keys: ["displayName", "isEnabled"],
+        }}
+        formData={languageDetailsData}
+        onSubmit={(data) => {
+          setLoading(true);
+          const formData = data.formData;
+          void putLanguageApi({
+            id: languageDetailsData.id || "",
+            requestBody: formData,
+          })
+            .then((res) => {
+              handlePutResponse(res, router);
+            })
+            .finally(() => {
+              setLoading(false);
+            });
+        }}
+        schema={$Volo_Abp_LanguageManagement_Dto_UpdateLanguageDto}
+        submitText={languageData["Edit.Save"]}
+        uiSchema={uiSchema}
+      />
+    </div>
+  );
+}
--- apps/web/src/app/[lang]/(main)/(core)/management/language-management/languages/page.tsx
+++ apps/web/src/app/[lang]/(main)/(core)/management/language-management/languages/page.tsx
@@ -5,6 +5,7 @@ import { getLanguagesApi } from "src/actions/core/AdministrationService/actions"
 import { getResourceData } from "src/language-data/core/AdministrationService";
 import { isUnauthorized } from "src/utils/page-policy/page-policy";
 import { isErrorOnRequest } from "src/utils/page-policy/utils";
+import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
 import LanguagesTable from "./_components/table";
 
 export default async function Page({
@@ -15,13 +16,20 @@ export default async function Page({
   searchParams: GetApiLanguageManagementLanguagesData;
 }) {
   const { lang } = params;
+  const { languageData } = await getResourceData(lang);
   await isUnauthorized({
     requiredPolicies: ["LanguageManagement.Languages"],
     lang,
   });
   const languagesResponse = await getLanguagesApi(searchParams);
-  if (isErrorOnRequest(languagesResponse, lang)) return;
-  const { languageData } = await getResourceData(lang);
+  if (isErrorOnRequest(languagesResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={languagesResponse.message}
+      />
+    );
+  }
 
   return (
     <LanguagesTable
