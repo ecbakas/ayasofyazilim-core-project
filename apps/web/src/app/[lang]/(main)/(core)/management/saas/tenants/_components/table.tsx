b12ed8167924c96d93ac33b86748b431641a5def
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-07 23:00:48 +0300

    feat:add tenant table

---
+++ apps/web/src/app/[lang]/(main)/(core)/management/saas/tenants/_components/table.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import type {
+  Volo_Abp_Application_Dtos_PagedResultDto_15,
+  Volo_Saas_Host_Dtos_EditionLookupDto,
+} from "@ayasofyazilim/saas/SaasService";
+import TanstackTable from "@repo/ayasofyazilim-ui/molecules/tanstack-table";
+import { useParams, useRouter } from "next/navigation";
+import type { SaasServiceResource } from "src/language-data/core/SaasService";
+import { useGrantedPolicies } from "src/providers/granted-policies";
+import { tableData } from "./tenants-table-data";
+
+function TenantsTable({
+  response,
+  languageData,
+  editionList,
+}: {
+  response: Volo_Abp_Application_Dtos_PagedResultDto_15;
+  languageData: SaasServiceResource;
+  editionList: Volo_Saas_Host_Dtos_EditionLookupDto[];
+}) {
+  const router = useRouter();
+  const { lang } = useParams<{ lang: string }>();
+  const { grantedPolicies } = useGrantedPolicies();
+
+  const columns = tableData.tenants.columns(
+    lang,
+    languageData,
+    grantedPolicies,
+  );
+  const table = tableData.tenants.table(
+    languageData,
+    router,
+    grantedPolicies,
+    editionList,
+  );
+
+  return (
+    <TanstackTable
+      {...table}
+      columns={columns}
+      data={response.items || []}
+      rowCount={response.totalCount}
+    />
+  );
+}
+export default TenantsTable;
