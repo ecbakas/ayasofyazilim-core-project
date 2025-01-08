09949f908dde734d9a44b4c9ac3b907169b7c2ee
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-08 14:12:33 +0300

    feat: add set password action to user page

---
+++ apps/web/src/app/[lang]/(main)/(core)/management/identity/users/[userId]/set-password/page.tsx
@@ -0,0 +1,43 @@
+"use server";
+
+import { getUserDetailsByIdApi } from "src/actions/core/IdentityService/actions";
+import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
+import { getResourceData } from "src/language-data/core/IdentityService";
+import { isUnauthorized } from "src/utils/page-policy/page-policy";
+import { isErrorOnRequest } from "src/utils/page-policy/utils";
+import Form from "./_components/form";
+
+export default async function Page({
+  params,
+}: {
+  params: { lang: string; userId: string };
+}) {
+  const { lang, userId } = params;
+  const { languageData } = await getResourceData(lang);
+  await isUnauthorized({
+    requiredPolicies: ["AbpIdentity.Users"],
+    lang,
+  });
+
+  const userDetailsResponse = await getUserDetailsByIdApi(userId);
+  if (isErrorOnRequest(userDetailsResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={userDetailsResponse.message}
+      />
+    );
+  }
+
+  return (
+    <>
+      <Form languageData={languageData} />
+      <div className="hidden" id="page-title">
+        {`${languageData.User} (${userDetailsResponse.data.userName})`}
+      </div>
+      <div className="hidden" id="page-description">
+        {languageData["User.SetPassword.Description"]}
+      </div>
+    </>
+  );
+}
