78e742010b51ce71725275f75f90fcd3af0e61a7
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-08 14:11:50 +0300

    feat: add sessions action to user page

---
+++ apps/web/src/app/[lang]/(main)/(core)/management/identity/users/[userId]/sessions/_components/sessions-table-data.tsx
@@ -0,0 +1,90 @@
+import type { Volo_Abp_Identity_IdentitySessionDto } from "@ayasofyazilim/saas/IdentityService";
+import { $Volo_Abp_Identity_IdentitySessionDto } from "@ayasofyazilim/saas/IdentityService";
+import type {
+  TanstackTableColumnLink,
+  TanstackTableCreationProps,
+} from "@repo/ayasofyazilim-ui/molecules/tanstack-table/types";
+import { tanstackTableCreateColumnsByRowData } from "@repo/ayasofyazilim-ui/molecules/tanstack-table/utils";
+import { InfoIcon, Trash } from "lucide-react";
+import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { handleDeleteResponse } from "src/actions/core/api-utils-client";
+import { deleteUserSessionsByIdApi } from "src/actions/core/IdentityService/delete-actions";
+import type { IdentityServiceResource } from "src/language-data/core/IdentityService";
+import DetailsInformation from "./details-information";
+
+type SessionsTable =
+  TanstackTableCreationProps<Volo_Abp_Identity_IdentitySessionDto>;
+
+const links: Partial<
+  Record<keyof Volo_Abp_Identity_IdentitySessionDto, TanstackTableColumnLink>
+> = {};
+
+const sessionsColumns = (
+  locale: string,
+  languageData: IdentityServiceResource,
+) => {
+  return tanstackTableCreateColumnsByRowData<Volo_Abp_Identity_IdentitySessionDto>(
+    {
+      rows: $Volo_Abp_Identity_IdentitySessionDto.properties,
+      languageData: {
+        languageData,
+        constantKey: "Form.User.Session",
+      },
+      config: {
+        locale,
+      },
+      links,
+    },
+  );
+};
+
+function sessionsTable(
+  languageData: IdentityServiceResource,
+  router: AppRouterInstance,
+) {
+  const table: SessionsTable = {
+    fillerColumn: "userName",
+    columnVisibility: {
+      type: "show",
+      columns: ["device", "deviceInfo", "signedIn", "lastAccessed"],
+    },
+    rowActions: [
+      {
+        type: "custom-dialog",
+        cta: languageData["User.Session.Detail"],
+        title: languageData["User.Session.Details"],
+        icon: InfoIcon,
+        actionLocation: "row",
+        content: (rowData) => (
+          <DetailsInformation
+            languageData={languageData}
+            sessionData={rowData}
+          />
+        ),
+      },
+      {
+        type: "confirmation-dialog",
+        cta: languageData["User.Session.Revokes"],
+        title: languageData["User.Session.Revoke"],
+        actionLocation: "row",
+        confirmationText: languageData["User.Session.Revokes"],
+        cancelText: languageData.Cancel,
+        description: languageData["User.Session.Assurance"],
+        icon: Trash,
+        onConfirm: (row) => {
+          void deleteUserSessionsByIdApi(row.id || "").then((response) => {
+            handleDeleteResponse(response, router);
+          });
+        },
+      },
+    ],
+  };
+  return table;
+}
+
+export const tableData = {
+  sessions: {
+    columns: sessionsColumns,
+    table: sessionsTable,
+  },
+};
---
+++ apps/web/src/app/[lang]/(main)/(core)/management/identity/users/[userId]/sessions/page.tsx
@@ -0,0 +1,65 @@
+"use server";
+
+import type { GetApiIdentitySessionsData } from "@ayasofyazilim/saas/IdentityService";
+import {
+  getSessionsApi,
+  getUserDetailsByIdApi,
+} from "src/actions/core/IdentityService/actions";
+import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
+import { getResourceData } from "src/language-data/core/IdentityService";
+import { isUnauthorized } from "src/utils/page-policy/page-policy";
+import { isErrorOnRequest } from "src/utils/page-policy/utils";
+import SessionsTable from "./_components/table";
+
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { lang: string; userId: string };
+  searchParams: GetApiIdentitySessionsData;
+}) {
+  const { lang, userId } = params;
+  const { languageData } = await getResourceData(lang);
+  await isUnauthorized({
+    requiredPolicies: ["AbpIdentity.Users"],
+    lang,
+  });
+
+  const userDetailsResponse = await getUserDetailsByIdApi(userId);
+  if (isErrorOnRequest(userDetailsResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={userDetailsResponse.message}
+      />
+    );
+  }
+
+  const userSessionsResponse = await getSessionsApi({
+    ...searchParams,
+    userId,
+  });
+  if (isErrorOnRequest(userSessionsResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={userSessionsResponse.message}
+      />
+    );
+  }
+
+  return (
+    <>
+      <SessionsTable
+        languageData={languageData}
+        response={userSessionsResponse.data}
+      />
+      <div className="hidden" id="page-title">
+        {`${languageData.User} (${userDetailsResponse.data.userName})`}
+      </div>
+      <div className="hidden" id="page-description">
+        {languageData["User.Session.Description"]}
+      </div>
+    </>
+  );
+}
