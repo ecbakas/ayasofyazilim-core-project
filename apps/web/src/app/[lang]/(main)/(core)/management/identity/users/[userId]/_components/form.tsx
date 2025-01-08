b8ee35dec50c8618fd9b69a6ee041ad106654cba
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-08 13:31:24 +0300

    feat: add create & edit forms of user page

---
+++ apps/web/src/app/[lang]/(main)/(core)/management/identity/users/[userId]/_components/form.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import type {
+  UniRefund_IdentityService_AssignableRoles_AssignableRoleDto,
+  Volo_Abp_Identity_IdentityRoleDto,
+  Volo_Abp_Identity_IdentityUserDto,
+  Volo_Abp_Identity_OrganizationUnitDto,
+  Volo_Abp_Identity_OrganizationUnitLookupDto,
+} from "@ayasofyazilim/saas/IdentityService";
+import { $Volo_Abp_Identity_IdentityUserUpdateDto } from "@ayasofyazilim/saas/IdentityService";
+import { ActionList } from "@repo/ayasofyazilim-ui/molecules/action-button";
+import ConfirmDialog from "@repo/ayasofyazilim-ui/molecules/confirm-dialog";
+import { SchemaForm } from "@repo/ayasofyazilim-ui/organisms/schema-form";
+import { createUiSchemaWithResource } from "@repo/ayasofyazilim-ui/organisms/schema-form/utils";
+import { CustomMultiSelectWidget } from "@repo/ayasofyazilim-ui/organisms/schema-form/widgets";
+import { Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import {
+  handleDeleteResponse,
+  handlePutResponse,
+} from "src/actions/core/api-utils-client";
+import { deleteUserByIdApi } from "src/actions/core/IdentityService/delete-actions";
+import { putUserApi } from "src/actions/core/IdentityService/put-actions";
+import type { IdentityServiceResource } from "src/language-data/core/IdentityService";
+import { useGrantedPolicies } from "src/providers/granted-policies";
+import isActionGranted from "src/utils/page-policy/action-policy";
+
+type UserFormDto = Volo_Abp_Identity_IdentityUserDto & {
+  organizationUnitIds?: string[] | null;
+};
+
+export default function Form({
+  languageData,
+  userDetailsData,
+  roleList,
+  organizationList,
+  userRoles,
+  userOrganizationUnits,
+}: {
+  languageData: IdentityServiceResource;
+  userDetailsData: Volo_Abp_Identity_IdentityUserDto;
+  roleList: UniRefund_IdentityService_AssignableRoles_AssignableRoleDto[];
+  organizationList: Volo_Abp_Identity_OrganizationUnitLookupDto[];
+  userRoles: Volo_Abp_Identity_IdentityRoleDto[];
+  userOrganizationUnits: Volo_Abp_Identity_OrganizationUnitDto[];
+}) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { grantedPolicies } = useGrantedPolicies();
+  const uiSchema = createUiSchemaWithResource({
+    schema: $Volo_Abp_Identity_IdentityUserUpdateDto,
+    resources: languageData,
+    name: "Form.User",
+    extend: {
+      roleNames: {
+        "ui:widget": "Role",
+      },
+      organizationUnitIds: {
+        "ui:widget": "OrganizationUnit",
+      },
+      isActive: {
+        "ui:widget": "switch",
+        "ui:className": "md:col-span-2",
+      },
+      lockoutEnabled: {
+        "ui:widget": "switch",
+        "ui:className": "md:col-span-2",
+      },
+      phoneNumberConfirmed: {
+        "ui:widget": "switch",
+        "ui:className": "md:col-span-2",
+      },
+      shouldChangePasswordOnNextLogin: {
+        "ui:widget": "switch",
+        "ui:className": "md:col-span-2",
+      },
+      "ui:className": "md:grid md:grid-cols-2 md:gap-2",
+    },
+  });
+  return (
+    <div className="flex flex-col gap-4 overflow-auto">
+      <ActionList>
+        {isActionGranted(["AbpIdentity.Users.Delete"], grantedPolicies) && (
+          <ConfirmDialog
+            closeProps={{
+              children: languageData.Cancel,
+            }}
+            confirmProps={{
+              variant: "destructive",
+              children: languageData.Delete,
+              onConfirm: () => {
+                setLoading(true);
+                void deleteUserByIdApi(userDetailsData.id || "")
+                  .then((res) => {
+                    handleDeleteResponse(res);
+                    if (res.type === "success") router.push(`../users`);
+                  })
+                  .finally(() => {
+                    setLoading(false);
+                  });
+              },
+              closeAfterConfirm: true,
+            }}
+            description={languageData["Delete.Assurance"]}
+            title={languageData["User.Delete"]}
+            triggerProps={{
+              children: (
+                <>
+                  <Trash2 className="mr-2 w-4" /> {languageData.Delete}
+                </>
+              ),
+              variant: "outline",
+            }}
+            type="with-trigger"
+          />
+        )}
+      </ActionList>
+      <SchemaForm<UserFormDto>
+        className="flex flex-col gap-4"
+        disabled={loading}
+        filter={{
+          type: "include",
+          sort: true,
+          keys: [
+            "userName",
+            "name",
+            "surname",
+            "email",
+            "roleNames",
+            "organizationUnitIds",
+            "phoneNumber",
+            "isActive",
+            "lockoutEnabled",
+            "phoneNumberConfirmed",
+            "shouldChangePasswordOnNextLogin",
+          ],
+        }}
+        formData={{
+          ...userDetailsData,
+          roleNames: userRoles.map((role) => role.name || ""),
+          organizationUnitIds: userOrganizationUnits.map((org) => org.id || ""),
+        }}
+        onSubmit={(data) => {
+          setLoading(true);
+          const formData = data.formData;
+          void putUserApi({
+            id: userDetailsData.id || "",
+            requestBody: {
+              ...formData,
+              userName: formData?.userName || "",
+              email: formData?.email || "",
+            },
+          })
+            .then((res) => {
+              handlePutResponse(res, router);
+            })
+            .finally(() => {
+              setLoading(false);
+            });
+        }}
+        schema={$Volo_Abp_Identity_IdentityUserUpdateDto}
+        submitText={languageData["Edit.Save"]}
+        uiSchema={uiSchema}
+        widgets={{
+          Role: CustomMultiSelectWidget({
+            optionList: roleList
+              .filter((role) => role.isAssignable)
+              .map((role) => ({
+                label: role.roleName || "",
+                value: role.roleName || "",
+              })),
+          }),
+          OrganizationUnit: CustomMultiSelectWidget({
+            optionList: organizationList.map((organization) => ({
+              label: organization.displayName || "",
+              value: organization.id || "",
+            })),
+          }),
+        }}
+      />
+    </div>
+  );
+}
---
+++ apps/web/src/app/[lang]/(main)/(core)/management/identity/users/[userId]/page.tsx
@@ -0,0 +1,97 @@
+"use server";
+
+import {
+  getAssignableRolesByCurrentUserApi,
+  getUserDetailsByIdApi,
+  getUsersAvailableOrganizationUnitsApi,
+  getUsersByIdOrganizationUnitsApi,
+  getUsersByIdRolesApi,
+} from "src/actions/core/IdentityService/actions";
+import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
+import { getResourceData } from "src/language-data/core/IdentityService";
+import { isUnauthorized } from "src/utils/page-policy/page-policy";
+import { isErrorOnRequest } from "src/utils/page-policy/utils";
+import Form from "./_components/form";
+
+export default async function Page({
+  params,
+}: {
+  params: { lang: string; userId: string };
+}) {
+  const { lang, userId } = params;
+  const { languageData } = await getResourceData(lang);
+  await isUnauthorized({
+    requiredPolicies: ["AbpIdentity.Users.Update"],
+    lang,
+  });
+
+  const userDetailsResponse = await getUserDetailsByIdApi(userId);
+  if (isErrorOnRequest(userDetailsResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={userDetailsResponse.message}
+      />
+    );
+  }
+
+  const rolesResponse = await getAssignableRolesByCurrentUserApi();
+  if (isErrorOnRequest(rolesResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={rolesResponse.message}
+      />
+    );
+  }
+
+  const organizationResponse = await getUsersAvailableOrganizationUnitsApi();
+  if (isErrorOnRequest(organizationResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={organizationResponse.message}
+      />
+    );
+  }
+
+  const userRolesResponse = await getUsersByIdRolesApi(userId);
+  if (isErrorOnRequest(userRolesResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={userRolesResponse.message}
+      />
+    );
+  }
+
+  const userOrganizationResponse =
+    await getUsersByIdOrganizationUnitsApi(userId);
+  if (isErrorOnRequest(userOrganizationResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={userOrganizationResponse.message}
+      />
+    );
+  }
+
+  return (
+    <>
+      <Form
+        languageData={languageData}
+        organizationList={organizationResponse.data.items || []}
+        roleList={rolesResponse.data}
+        userDetailsData={userDetailsResponse.data}
+        userOrganizationUnits={userOrganizationResponse.data}
+        userRoles={userRolesResponse.data.items || []}
+      />
+      <div className="hidden" id="page-title">
+        {`${languageData.User} (${userDetailsResponse.data.userName})`}
+      </div>
+      <div className="hidden" id="page-description">
+        {languageData["User.Update.Description"]}
+      </div>
+    </>
+  );
+}
