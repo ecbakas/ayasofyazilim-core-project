d97234294b2b40e7975e3e72bc5f0ea71f66aa1d
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-21 13:46:48 +0300

    chore: use error component

--- apps/web/src/app/[lang]/(main)/(core)/management/openiddict/applications/new/page.tsx
+++ apps/web/src/app/[lang]/(main)/(core)/management/openiddict/applications/new/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { getAllScopesApi } from "src/actions/core/IdentityService/actions";
+import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
 import { getResourceData } from "src/language-data/core/IdentityService";
 import { isUnauthorized } from "src/utils/page-policy/page-policy";
 import { isErrorOnRequest } from "src/utils/page-policy/utils";
@@ -8,13 +9,20 @@ import Form from "./_components/form";
 
 export default async function Page({ params }: { params: { lang: string } }) {
   const { lang } = params;
+  const { languageData } = await getResourceData(lang);
   await isUnauthorized({
     requiredPolicies: ["OpenIddictPro.Scope.Create"],
     lang,
   });
   const scopesResponse = await getAllScopesApi();
-  if (isErrorOnRequest(scopesResponse, lang)) return;
-  const { languageData } = await getResourceData(lang);
+  if (isErrorOnRequest(scopesResponse, lang, false)) {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={scopesResponse.message}
+      />
+    );
+  }
   return (
     <>
       <Form languageData={languageData} scopeList={scopesResponse.data} />
