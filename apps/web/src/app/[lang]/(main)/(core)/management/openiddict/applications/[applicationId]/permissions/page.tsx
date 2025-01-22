d00eeb963a2478da0526301da0b5202ebcc261a1
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-22 09:28:00 +0300

    fix(applications): change the names to be correct

--- apps/web/src/app/[lang]/(main)/(core)/management/openiddict/applications/[applicationId]/permissions/page.tsx
+++ apps/web/src/app/[lang]/(main)/(core)/management/openiddict/applications/[applicationId]/permissions/page.tsx
@@ -51,7 +51,7 @@ export default async function Page({
         languageData={languageData}
       />
       <div className="hidden" id="page-title">
-        {`${languageData.Role} (${applicationDetailsResponse.data.displayName})`}
+        {`${languageData.Application} (${applicationDetailsResponse.data.displayName})`}
       </div>
       <div className="hidden" id="page-description">
         {languageData["Application.Permissions.Description"]}
