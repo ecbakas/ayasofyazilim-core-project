993d8d6f538fbb86e9fff6a16e8e15249b166adb
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-23 10:37:06 +0300

    feat: add the section of user and role unit

--- apps/web/src/app/[lang]/(main)/(core)/management/identity/organization/organization.tsx
+++ apps/web/src/app/[lang]/(main)/(core)/management/identity/organization/organization.tsx
@@ -3,8 +3,17 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { toast } from "@/components/ui/sonner";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import type {
   PagedResultDto_OrganizationUnitWithDetailsDto,
+  Volo_Abp_Identity_IdentityRoleDto,
   Volo_Abp_Identity_IdentityUserDto,
   Volo_Abp_Identity_OrganizationUnitWithDetailsDto,
 } from "@ayasofyazilim/saas/IdentityService";
@@ -16,6 +25,8 @@ import { createZodObject } from "@repo/ayasofyazilim-ui/lib/create-zod-object";
 import type { TableActionCustomDialog } from "@repo/ayasofyazilim-ui/molecules/dialog";
 import AutoformDialog from "@repo/ayasofyazilim-ui/molecules/dialog";
 import { TreeView } from "@repo/ayasofyazilim-ui/molecules/tree-view";
+import { SectionNavbarBase } from "@repo/ayasofyazilim-ui/templates/section-layout";
+import { Trash2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import type { TreeViewElement } from "node_modules/@repo/ayasofyazilim-ui/src/molecules/tree-view/tree-view-api";
 import { useEffect, useState } from "react";
@@ -25,8 +36,15 @@ import {
   handlePostResponse,
   handlePutResponse,
 } from "src/actions/core/api-utils-client";
-import { getOrganizationUnitsByIdMembersApi } from "src/actions/core/IdentityService/actions";
-import { deleteOrganizationUnitsApi } from "src/actions/core/IdentityService/delete-actions";
+import {
+  getOrganizationUnitsByIdMembersApi,
+  getOrganizationUnitsByIdRolesApi,
+} from "src/actions/core/IdentityService/actions";
+import {
+  deleteOrganizationUnitsApi,
+  deleteOrganizationUnitsByIdMembersByMemberIdApi,
+  deleteOrganizationUnitsByIdRolesByRoleIdApi,
+} from "src/actions/core/IdentityService/delete-actions";
 import { postOrganizationUnitsApi } from "src/actions/core/IdentityService/post-actions";
 import {
   putOrganizationUnitsByIdApi,
@@ -86,6 +104,10 @@ export default function OrganizationComponent({
   const [unitUsers, setUnitUsers] = useState<
     Volo_Abp_Identity_IdentityUserDto[]
   >([]);
+  const [unitRoles, setUnitRoles] = useState<
+    Volo_Abp_Identity_IdentityRoleDto[]
+  >([]);
+  const [activeTab, setActiveTab] = useState("Users");
 
   function getUnitUsers() {
     void getOrganizationUnitsByIdMembersApi({
@@ -97,8 +119,19 @@ export default function OrganizationComponent({
     });
   }
 
+  function getUnitRoles() {
+    void getOrganizationUnitsByIdRolesApi({
+      id: selectedUnitId || "",
+    }).then((res) => {
+      if (res.type === "success") {
+        setUnitRoles(res.data.items || []);
+      }
+    });
+  }
+
   useEffect(() => {
     getUnitUsers();
+    getUnitRoles();
   }, [selectedUnitId]);
 
   const handleAddUnit = (parentUnitId: string | null) => {
@@ -179,6 +212,7 @@ export default function OrganizationComponent({
               return i;
             });
             setOrganizationUnits(newOrganizationUnits);
+            setSelectedUnitId(selectedUnitId);
             setTriggerData({});
             setOpen(false);
           }
@@ -292,6 +326,30 @@ export default function OrganizationComponent({
     setOpen(true);
   };
 
+  const handleDeleteUser = (userId: string) => {
+    void deleteOrganizationUnitsByIdMembersByMemberIdApi({
+      id: selectedUnitId || "",
+      memberId: userId || "",
+    }).then((res) => {
+      handleDeleteResponse(res);
+      if (res.type === "success") {
+        getUnitUsers();
+      }
+    });
+  };
+
+  const handleDeleteRole = (roleId: string) => {
+    void deleteOrganizationUnitsByIdRolesByRoleIdApi({
+      id: selectedUnitId || "",
+      roleId: roleId || "",
+    }).then((res) => {
+      handleDeleteResponse(res);
+      if (res.type === "success") {
+        getUnitRoles();
+      }
+    });
+  };
+
   return (
     <>
       <div className="flex min-h-[60vh] w-full flex-row">
@@ -352,6 +410,107 @@ export default function OrganizationComponent({
               />
             ) : (
               <p>No organization units available</p>
+            )}
+          </CardContent>
+        </Card>
+
+        <Card className="m-2 max-h-[70vh] w-1/2 overflow-y-auto">
+          <CardContent>
+            <SectionNavbarBase
+              activeSectionId={activeTab}
+              navClassName="p-0"
+              navContainerClassName="shadow-none"
+              onSectionChange={(newActiveSection) => {
+                setActiveTab(newActiveSection);
+              }}
+              sections={[
+                { id: "Users", name: "Users" },
+                { id: "Roles", name: "Roles" },
+              ]}
+              showContentInSamePage
+            />
+            {selectedUnitId ? (
+              <div>
+                <div className="mb-4 flex items-center justify-between">
+                  <div className="flex w-full justify-end">
+                    {activeTab === "Users" ? (
+                      <Button className="bg-primary rounded px-4 py-2 text-white">
+                        + Add user
+                      </Button>
+                    ) : (
+                      <Button className="bg-primary rounded px-4 py-2 text-white">
+                        + Add role
+                      </Button>
+                    )}
+                  </div>
+                </div>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      {activeTab === "Users" ? (
+                        <>
+                          <TableHead>User Name</TableHead>
+                          <TableHead>Email Address</TableHead>
+                        </>
+                      ) : (
+                        <TableHead>Role Name</TableHead>
+                      )}
+                      <TableHead className="text-right" />
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {activeTab === "Users" && unitUsers.length > 0
+                      ? unitUsers.map((user) => (
+                          <TableRow key={user.id}>
+                            <TableCell>{user.userName}</TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell className="text-right">
+                              <Button
+                                onClick={() => {
+                                  handleDeleteUser(user.id || "");
+                                }}
+                                variant="link"
+                              >
+                                <Trash2 className="h-4 w-4" />
+                              </Button>
+                            </TableCell>
+                          </TableRow>
+                        ))
+                      : activeTab === "Users" && (
+                          <TableRow>
+                            <TableCell>No data available</TableCell>
+                          </TableRow>
+                        )}
+                    {activeTab === "Roles" && unitRoles.length > 0
+                      ? unitRoles.map((role) => (
+                          <TableRow key={role.id}>
+                            <TableCell>{role.name}</TableCell>
+                            <TableCell className="text-right">
+                              <Button
+                                onClick={() => {
+                                  handleDeleteRole(role.id || "");
+                                }}
+                                variant="link"
+                              >
+                                <Trash2 className="h-4 w-4" />
+                              </Button>
+                            </TableCell>
+                          </TableRow>
+                        ))
+                      : activeTab === "Roles" && (
+                          <TableRow>
+                            <TableCell>No data available</TableCell>
+                          </TableRow>
+                        )}
+                  </TableBody>
+                </Table>
+                <p className="mt-10 text-sm">
+                  {activeTab === "Users" ? unitUsers.length : unitRoles.length}{" "}
+                  total
+                </p>
+              </div>
+            ) : (
+              <p>Select an organization unit to view details</p>
             )}
           </CardContent>
         </Card>
