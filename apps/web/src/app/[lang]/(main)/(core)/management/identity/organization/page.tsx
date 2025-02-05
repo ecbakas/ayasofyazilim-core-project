459e68d7e302de22965517bfb5c4eef40f9b2555
Eren <ahmeterenodaci@gmail.com>
2025-02-05 15:32:35 +0300

    feat: enhance signUpServer to include tenantId and update imports for policy utilities

--- apps/web/src/app/[lang]/(main)/(core)/management/identity/organization/page.tsx
+++ apps/web/src/app/[lang]/(main)/(core)/management/identity/organization/page.tsx
@@ -1,10 +1,10 @@
 "use server";
 
 import type {GetApiIdentityRolesData, GetApiIdentityUsersData} from "@ayasofyazilim/saas/IdentityService";
+import {isUnauthorized} from "@repo/utils/policies";
 import {getAllOrganizationUnitsApi, getRolesApi, getUsersApi} from "src/actions/core/IdentityService/actions";
 import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
 import {getResourceData} from "src/language-data/core/IdentityService";
-import {isUnauthorized} from "src/utils/page-policy/page-policy";
 import {isErrorOnRequest} from "src/utils/page-policy/utils";
 import OrganizationComponent from "./organization";
 
