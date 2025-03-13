2645d94b29035cacbaf5d4a5badaed08373b7a52
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-03-13 10:16:45 +0300

    feat(novu): rename workflow ID type for clarity and extend to project workflows

--- packages/actions/core/NovuService/actions.ts
+++ packages/actions/core/NovuService/actions.ts
@@ -3,10 +3,11 @@ import {Novu} from "@novu/api";
 
 import {TriggerEventRequestDto} from "@novu/api/models/components";
 
-export type WorkFlowId = "workflow-1" | "workflow-2" | "workflow-3";
+export type CoreWorkFlowIds = "workflow-1" | "workflow-2" | "workflow-3";
+export type ProjectWorkFlowIds = "" | "";
 
 export type TriggerNovuNotification<P> = {
-  workflowId: WorkFlowId;
+  workflowId: CoreWorkFlowIds | ProjectWorkFlowIds;
   payload: P;
   to: TriggerEventRequestDto["to"];
 };
