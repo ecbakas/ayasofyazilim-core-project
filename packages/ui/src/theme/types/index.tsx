34065bd708d3c0d01204230df5d9b42f97e807fd
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-06-24 13:29:52 +0300

    chore: update subproject commits and improve breadcrumb component functionality

--- packages/ui/src/theme/types/index.tsx
+++ packages/ui/src/theme/types/index.tsx
@@ -1,4 +1,3 @@
-
 export type NavbarItemsFromDB = {
   key: string;
   parentNavbarItemKey: string | null;
@@ -9,6 +8,7 @@ export type NavbarItemsFromDB = {
   displayOrder: number;
   requiredPolicies?: string[];
   hidden?: boolean;
+  split?: "top" | "bottom" | "both";
 };
 export type BreadcrumbItemType = NavbarItemsFromDB & {
   subNavbarItems: NavbarItemsFromDB[];
@@ -17,9 +17,9 @@ export type BreadcrumbItemType = NavbarItemsFromDB & {
 export type NavbarItemType = NavbarItemsFromDB & {
   subNavbarItems: NavbarItemType[] | null;
 };
-type ProfileMenuLink = { name: string; icon: JSX.Element } & (
-  | { onClick: () => void | Promise<unknown>; href: undefined }
-  | { href: string; onClick: undefined }
+type ProfileMenuLink = {name: string; icon: JSX.Element} & (
+  | {onClick: () => void | Promise<unknown>; href: undefined}
+  | {href: string; onClick: undefined}
 );
 
 export type ProfileMenuProps = {
@@ -35,5 +35,3 @@ export type ProfileMenuProps = {
   };
   menuTitle: string;
 };
-
-
